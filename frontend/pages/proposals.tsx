bc0466fef3d0389b17de50e1d1acf093db216173
Matthew Brown <mattfdbrown@gmail.com>
2021-08-26 11:25:36 -0700

    pull out ProfileDropdown

--- frontend/pages/proposals.tsx
+++ frontend/pages/proposals.tsx
@@ -16,6 +16,7 @@ import {
   DocumentTextIcon,
   PencilIcon,
 } from "@heroicons/react/solid";
+import ProfileDropdown from "components/ProfileDropdown/ProfileDropdown";
 import Sidebar from "components/Sidebar/Sidebar";
 import Img from "next/image";
 import Link from "next/link";
@@ -145,6 +146,7 @@ const Proposals: NextPage = () => {
       </Transition.Root>
 
       <Sidebar />
+
       <div className="flex flex-col w-0 flex-1 overflow-hidden">
         <div className="relative z-10 flex-shrink-0 flex h-16 bg-gray shadow">
           <button
@@ -166,48 +168,7 @@ const Proposals: NextPage = () => {
                 <BellIcon className="h-6 w-6" aria-hidden="true" />
               </button>
 
-              {/* Profile dropdown */}
-              <Menu as="div" className="ml-3 relative">
-                <div>
-                  <Menu.Button className="max-w-xs bg-white flex items-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                    <span className="sr-only">Open user menu</span>
-                    <Img
-                      className="h-8 w-8 rounded-full"
-                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
-                      width="24px"
-                      height="24px"
-                    />
-                  </Menu.Button>
-                </div>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-100"
-                  enterFrom="transform opacity-0 scale-95"
-                  enterTo="transform opacity-100 scale-100"
-                  leave="transition ease-in duration-75"
-                  leaveFrom="transform opacity-100 scale-100"
-                  leaveTo="transform opacity-0 scale-95"
-                >
-                  <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                    {userNavigation.map((item) => (
-                      <Menu.Item key={item.name}>
-                        {({ active }) => (
-                          <a
-                            href={item.href}
-                            className={classNames(
-                              active ? "bg-blue-50" : "",
-                              "block px-4 py-2 text-sm text-blue-700"
-                            )}
-                          >
-                            {item.name}
-                          </a>
-                        )}
-                      </Menu.Item>
-                    ))}
-                  </Menu.Items>
-                </Transition>
-              </Menu>
+              <ProfileDropdown />
             </div>
           </div>
         </div>
@@ -217,7 +178,7 @@ const Proposals: NextPage = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
               {/* This changes sidebar text colour*/}
               <h1 className="text-2xl font-semibold text-blue-400">
-                Governance Overview{" "}
+                Proposals
               </h1>
             </div>
             <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
