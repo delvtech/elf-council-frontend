53598debba27f72e146ca19ad8f4c90e81d88bce
Matthew Brown <mattfdbrown@gmail.com>
2021-08-26 11:37:06 -0700

    add mobile Sidebar to Sidebar component

--- frontend/pages/proposals.tsx
+++ frontend/pages/proposals.tsx
@@ -1,54 +1,10 @@
 import type { NextPage } from "next";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import "tailwindcss/tailwind.css";
 
-import { Dialog, Menu, Transition } from "@headlessui/react";
-import {
-  BellIcon,
-  HomeIcon,
-  MenuAlt2Icon,
-  UserAddIcon,
-  UserGroupIcon,
-  XIcon,
-} from "@heroicons/react/outline";
-import {
-  ChatAltIcon,
-  DocumentTextIcon,
-  PencilIcon,
-} from "@heroicons/react/solid";
+import { BellIcon, MenuAlt2Icon } from "@heroicons/react/outline";
 import ProfileDropdown from "components/ProfileDropdown/ProfileDropdown";
 import Sidebar from "components/Sidebar/Sidebar";
-import Img from "next/image";
-import Link from "next/link";
-
-const navigation = [
-  { name: "Overview", href: "/", icon: HomeIcon, current: true },
-  { name: "Proposals", href: "/proposals", icon: PencilIcon, current: false },
-  {
-    name: "Leaderboard",
-    href: "/leaderboard",
-    icon: UserGroupIcon,
-    current: false,
-  },
-  { name: "Delegate", href: "/delegate", icon: UserAddIcon, current: false },
-  { name: "Forum", href: "/forum", icon: ChatAltIcon, current: false },
-  {
-    name: "Resources",
-    href: "/resources",
-    icon: DocumentTextIcon,
-    current: false,
-  },
-];
-
-const userNavigation = [
-  { name: "Your Profile", href: "#" },
-  { name: "Settings", href: "#" },
-  { name: "Sign out", href: "#" },
-];
-
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const Proposals: NextPage = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -56,96 +12,7 @@ const Proposals: NextPage = () => {
   return (
     /* changes the background colour of main overview (not sidebar) */
     <div className="h-screen flex overflow-hidden bg-blue-50">
-      <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 flex z-40 md:hidden"
-          onClose={setSidebarOpen}
-        >
-          <Transition.Child
-            as={Fragment}
-            enter="transition-opacity ease-linear duration-300"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity ease-linear duration-300"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-50 bg-opacity-75" />
-          </Transition.Child>
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <div className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-blue-500">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute top-0 right-0 -mr-12 pt-2">
-                  <button
-                    type="button"
-                    className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                    onClick={() => setSidebarOpen(false)}
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <XIcon
-                      className="h-6 w-6 text-blue-400"
-                      aria-hidden="true"
-                    />
-                  </button>
-                </div>
-              </Transition.Child>
-              <div className="flex-shrink-0 flex items-center px-4">
-                <Img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/workflow-logo-indigo-300-mark-white-text.svg"
-                  alt="Workflow"
-                  width="24px"
-                  height="24px"
-                />
-              </div>
-              <div className="mt-5 flex-1 h-0 overflow-y-auto">
-                <nav className="px-2 space-y-1">
-                  {navigation.map((item) => (
-                    <Link key={item.name} href={item.href}>
-                      <a
-                        className={classNames(
-                          item.current
-                            ? "bg-blue-500 text-blue"
-                            : "text-blue-100 hover:bg-blue-600",
-                          "group flex items-center px-2 py-2 text-base font-medium rounded-md"
-                        )}
-                      >
-                        <item.icon
-                          className="mr-4 flex-shrink-0 h-6 w-6 text-blue-400"
-                          aria-hidden="true"
-                        />
-                        {item.name}
-                      </a>
-                    </Link>
-                  ))}
-                </nav>
-              </div>
-            </div>
-          </Transition.Child>
-          <div className="flex-shrink-0 w-14" aria-hidden="true">
-            {/* Dummy element to force sidebar to shrink to fit close icon */}
-          </div>
-        </Dialog>
-      </Transition.Root>
-
-      <Sidebar />
+      <Sidebar isSidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
       <div className="flex flex-col w-0 flex-1 overflow-hidden">
         <div className="relative z-10 flex-shrink-0 flex h-16 bg-gray shadow">
