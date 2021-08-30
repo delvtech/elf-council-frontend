682a099c0c8563178eb973b6183711357ec63d5a
tinahaibodi <27844500+tinahaibodi@users.noreply.github.com>
2021-08-30 01:06:55 -0500

    fixing quorum chart

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "028bfd2478f1f9530ee0";
+/******/ 	var hotCurrentHash = "05d17d0f11e763a2c23d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
