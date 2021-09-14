456ec88291c20473d8f5d61d07d0b0fe5463ad64
Matthew Brown <mattfdbrown@gmail.com>
2021-09-14 15:52:49 -0700

    add comment to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  // allows tailwind to tree shake all css not included in the files found in this array.
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
