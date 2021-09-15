c0c3630f2241c10ed74fd3e4ec6c0b0fcd7e1831
Matthew Brown <mattfdbrown@gmail.com>
2021-09-15 11:07:39 -0700

    add PageView

---
+++ frontend/pages/components/PageView.tsx
@@ -0,0 +1,23 @@
+import React, { ReactElement, ReactNode } from "react";
+
+import Header from "components/Header";
+import { Sidebar } from "components/Sidebar";
+
+interface PageViewProps {
+  children?: ReactNode;
+}
+
+export default function PageView(props: PageViewProps): ReactElement {
+  const { children } = props;
+  return (
+    <div>
+      <div className="flex w-screen h-screen">
+        <Sidebar />
+        <div className="w-screen ">
+          <Header />
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
