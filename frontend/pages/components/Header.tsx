b29468a5cb7a167b6e295a03963562af0a168e44
Matthew Brown <mattfdbrown@gmail.com>
2021-09-16 11:31:06 -0700

    add more tw()

--- frontend/pages/components/Header.tsx
+++ frontend/pages/components/Header.tsx
@@ -3,15 +3,26 @@ import React, { ReactElement } from "react";
 import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import { t } from "ttag";
+import tw from "elf-tailwindcss-classnames";
 
 function Header(): ReactElement {
   return (
-    <div className="flex shadow-sm bg-gray-50  p-4 justify-between  ">
-      <div className="flex space-x-3  "></div>
-      <div className="flex space-x-4 text-gray-400 mr-3">
+    <div
+      className={tw(
+        "flex",
+        "shadow-sm",
+        "bg-gray-50",
+        "p-4",
+        "justify-between"
+      )}
+    >
+      <div className={tw("flex", "space-x-3")}></div>
+      <div className={tw("flex", "space-x-4", "text-gray-400", "mr-3")}>
         <NotificationsIcon />
         <ExitToAppIcon />
-        <p className="text-blue-400 font-semibold">{t`Connect Wallet`}</p>
+        <p
+          className={tw("text-blue-400", "font-semibold")}
+        >{t`Connect Wallet`}</p>
       </div>
     </div>
   );
