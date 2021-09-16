ba908cd1560038f10cfc34e8747d1403e8459c0a
Matthew Brown <mattfdbrown@gmail.com>
2021-09-16 10:41:45 -0700

    update Card to tw()

--- frontend/pages/components/Card.tsx
+++ frontend/pages/components/Card.tsx
@@ -4,21 +4,26 @@ import DescriptionIcon from "@material-ui/icons/Description";
 import GavelIcon from "@material-ui/icons/Gavel";
 import GradeIcon from "@material-ui/icons/Grade";
 import MonetizationOnIcon from "@material-ui/icons/MonetizationOn";
+import tw from "elf-tailwindcss-classnames";
 
-const Style = "text-white text-xs";
+const iconStyle = tw("text-white", "text-xs");
 
 const arrayIcon = [
-  <GavelIcon key="gavel" fontSize="small" className={Style} />,
-  <GradeIcon key="grade" fontSize="small" className={Style} />,
-  <DescriptionIcon key="description" fontSize="small" className={Style} />,
-  <MonetizationOnIcon key="monetization" fontSize="small" className={Style} />,
+  <GavelIcon key="gavel" fontSize="small" className={iconStyle} />,
+  <GradeIcon key="grade" fontSize="small" className={iconStyle} />,
+  <DescriptionIcon key="description" fontSize="small" className={iconStyle} />,
+  <MonetizationOnIcon
+    key="monetization"
+    fontSize="small"
+    className={iconStyle}
+  />,
 ];
 
 const Color = [
-  "from-blue-400 to-blue-300",
-  "from-blue-400 to-blue-300",
-  "from-blue-400 to-blue-300",
-  "from-blue-400 to-blue-300",
+  tw("from-blue-400", "to-blue-300"),
+  tw("from-blue-400", "to-blue-300"),
+  tw("from-blue-400", "to-blue-300"),
+  tw("from-blue-400", "to-blue-300"),
 ];
 
 interface CardProps {
@@ -32,19 +37,44 @@ export function Card(props: CardProps): ReactElement {
 
   return (
     <div
-      className={`transform hover:scale-110 cursor-pointer transition delay-100 w-3/12  p-2 py-4 shadow-xl  border rounded-xl bg-gradient-to-r ${
+      className={tw(
+        "transform-gpu",
+        "hover:scale-110",
+        "cursor-pointer",
+        "transition",
+        "delay-100",
+        "w-3/12",
+        "p-2",
+        "py-4",
+        "shadow-xl",
+        "border",
+        "rounded-xl",
+        "bg-gradient-to-r",
         Color[props.icon]
-      }`}
+      )}
     >
-      <div className="flex justify-between">
-        <div></div>
-        <div className=" w-10  h-10 flex items-center justify-center  bg-gray-300 rounded-xl m-1  bg-opacity-30">
+      <div className={tw("flex", "justify-end")}>
+        <div
+          className={tw(
+            "w-10",
+            "h-10",
+            "flex",
+            "items-center",
+            "justify-center",
+            "bg-gray-300",
+            "rounded-xl",
+            "m-1",
+            "bg-opacity-30"
+          )}
+        >
           {arrayIcon[icon]}
         </div>
       </div>
-      <p className="text-gray-200 text-xs  ">{title}</p>
-      <p className="text-gray-50 text-lg  font-semibold  ">{balance}</p>
-      <p className="text-gray-300  text-sm ">${increasedBalance}</p>
+      <p className={tw("text-gray-200", "text-xs")}>{title}</p>
+      <p className={tw("text-gray-50", "text-lg", "font-semibold")}>
+        {balance}
+      </p>
+      <p className={tw("text-gray-300", "text-sm")}>${increasedBalance}</p>
     </div>
   );
 }
