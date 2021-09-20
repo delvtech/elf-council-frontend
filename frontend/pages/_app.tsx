5d81eb9967ea98117e7aa7bc59325ed27679fffa
Matthew Brown <mattfdbrown@gmail.com>
2021-09-20 11:51:55 -0700

    move query client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,14 +1,14 @@
 import React, { ReactElement } from "react";
-import { QueryClient, QueryClientProvider } from "react-query";
+import { QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import "styles/globals.css";
 
 import { Web3ReactProvider } from "@web3-react/core";
 import { getEthereumProviderLibrary } from "elf/efi/getEthereumProviderLibrary";
+import { queryClient } from "elf/efi/queryClient";
 import { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <Web3ReactProvider getLibrary={getEthereumProviderLibrary}>
