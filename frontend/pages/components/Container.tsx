a0af3bb1dcafe8381db3d06b25694fb7a1d02b94
Matthew Brown <mattfdbrown@gmail.com>
2021-09-19 18:47:53 -0700

    move hook to its own component

--- frontend/pages/components/Container.tsx
+++ frontend/pages/components/Container.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { request, gql } from "graphql-request";
+
 import DescriptionIcon from "@material-ui/icons/Description";
 import GavelIcon from "@material-ui/icons/Gavel";
 import GradeIcon from "@material-ui/icons/Grade";
@@ -8,8 +8,8 @@ import Card from "components/Card";
 import Middle from "components/Middle";
 import RightBar from "components/RightBar";
 import tw from "elf-tailwindcss-classnames";
+import { useProposals } from "elf/efi/proposals/useProposals";
 import { t } from "ttag";
-import { useQuery } from "react-query";
 
 const iconStyle = tw("text-white", "text-xs");
 
@@ -86,39 +86,3 @@ export function Container(): ReactElement {
 }
 
 export default Container;
-function useProposals() {
-  return useQuery(["proposals"], async () => {
-    const { proposals } = await request(
-      endpoint,
-      gql`
-        query {
-          proposals(
-            first: 20
-            skip: 0
-            # where: { space_in: ["balancer", "yam.eth"], state: "closed" }
-            orderBy: "created"
-            orderDirection: desc
-          ) {
-            id
-            title
-            body
-            choices
-            start
-            end
-            snapshot
-            state
-            author
-            space {
-              id
-              name
-            }
-          }
-        }
-      `
-    );
-
-    return proposals;
-  });
-}
-
-const endpoint = "https://hub.snapshot.org/graphql";
