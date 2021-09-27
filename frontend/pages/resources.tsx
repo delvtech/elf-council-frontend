dc0f80bb274c8629b057582a87227e5be2d6e92a
Danny Delott <dannydelott@gmail.com>
2021-09-27 16:00:30 -0700

    Add Button component to base/

--- frontend/pages/resources.tsx
+++ frontend/pages/resources.tsx
@@ -1,6 +1,20 @@
 import React, { ReactElement } from "react";
 import PageView from "src/efi-ui/app/PageView";
+import Button from "src/efi-ui/base/Button/Button";
+import ResourcesHeader from "src/efi-ui/resources/ResourcesHeader";
+import tw from "src/elf-tailwindcss-classnames";
+import { t } from "ttag";
 
 export default function Resources(): ReactElement {
-  return <PageView></PageView>;
+  return (
+    <PageView>
+      <div className={tw("flex", "items-center", "justify-between")}>
+        <ResourcesHeader />
+        <div className={"space-x-4"}>
+          <Button>{t`View Template`}</Button>
+          <Button>{t`New Proposal`}</Button>
+        </div>
+      </div>
+    </PageView>
+  );
 }
