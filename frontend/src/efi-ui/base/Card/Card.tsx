2e136639d4d6168467b3c572b030e6e54d03a17a
Danny Delott <dannydelott@gmail.com>
2021-09-27 23:23:15 -0700

    Update card template

--- frontend/src/efi-ui/base/Card/Card.tsx
+++ frontend/src/efi-ui/base/Card/Card.tsx
@@ -1,4 +1,4 @@
-// See: https://tailwindui.com/components/application-ui/headings/card-headings
+// See: https://tailwindui.com/components/application-ui/layout/panels#component-415761fd4b5592742ec78ce4c638973e
 
 import { ReactElement, ReactNode } from "react";
 import tw from "src/elf-tailwindcss-classnames";
@@ -10,17 +10,8 @@ interface CardProps {
 export default function Card(props: CardProps): ReactElement {
   const { children } = props;
   return (
-    <div
-      className={tw(
-        "bg-white",
-        "px-4",
-        "py-5",
-        "border-b",
-        "border-gray-200",
-        "sm:px-6"
-      )}
-    >
-      {children}
+    <div className={tw("bg-white", "overflow-hidden", "shadow", "rounded-lg")}>
+      <div className={tw("px-4", "py-5", "sm:p-6")}>{children}</div>
     </div>
   );
 }
