b67e960938e5c42b9a7b4c64da6041f97acd2199
Danny Delott <dannydelott@gmail.com>
2021-09-27 23:25:08 -0700

    Update Well to match wireframe

--- frontend/src/efi-ui/base/Well/Well.tsx
+++ frontend/src/efi-ui/base/Well/Well.tsx
@@ -1,4 +1,5 @@
 // See: https://tailwindui.com/components/application-ui/layout/panels#component-b6db9f2e4b73f313be4bd2b76633e93a
+
 import classNames from "classnames";
 import { ReactElement, ReactNode } from "react";
 import tw from "src/elf-tailwindcss-classnames";
@@ -9,7 +10,7 @@ interface WellProps {
 }
 export default function Well({ className, children }: WellProps): ReactElement {
   return (
-    <div className={tw("bg-blue-100", "overflow-hidden", "rounded-lg")}>
+    <div className={tw("bg-blue-100", "overflow-hidden", "rounded-2xl")}>
       <div className={classNames(tw("px-4", "py-5", "sm:p-6"), className)}>
         {children}
       </div>
