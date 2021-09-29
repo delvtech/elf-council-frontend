29abadb0aa403d2a6689453eb3b2a9acbc31ef41
Danny Delott <dannydelott@gmail.com>
2021-09-29 16:19:21 -0700

    Build latest tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,6 @@
 module.exports = {
   // allows tailwind to tree shake all css not included in the files found in this array.
-  purge: {
-    // TODO: Turn this off in development, and on in production. Maybe a
-    // separate tailwind.config.prod.js file that extends this one?
-    // see https://github.com/tailwindlabs/tailwindcss/discussions/2963#discussioncomment-145462
-    // enabled: true,
-
-    content: ["./pages/**/*.tsx", "./src/**/*.tsx"],
-  },
+  purge: ["./pages/**/*.tsx", "./src/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
