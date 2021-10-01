ad322c2629a5d45fc052b65970d29b07c23366fe
Danny Delott <dannydelott@gmail.com>
2021-10-01 13:11:51 -0700

    Add minimal styling to Button and AnchorButton (#36)

--- frontend/src/efi-ui/base/Button/AnchorButton.tsx
+++ frontend/src/efi-ui/base/Button/AnchorButton.tsx
@@ -1,12 +1,14 @@
 // See: https://tailwindui.com/components/application-ui/elements/buttons
 
 import { MouseEventHandler, ReactElement, ReactNode } from "react";
-import { TAILWIND_BUTTON_CLASS } from "src/efi-ui/base/Button/styles";
+import { getButtonClass } from "src/efi-ui/base/Button/styles";
 
 interface AnchorButtonProps {
   children: ReactNode;
   onClick?: MouseEventHandler<HTMLAnchorElement>;
   href?: string;
+
+  minimal?: boolean;
   // TODO: Add rounded prop
 }
 
@@ -14,13 +16,15 @@ export default function AnchorButton({
   onClick,
   href,
   children,
+  minimal,
 }: AnchorButtonProps): ReactElement {
+  const buttonClass = getButtonClass({ minimal });
   return (
     <a
       href={href}
       role="button"
       onClick={onClick}
-      className={TAILWIND_BUTTON_CLASS}
+      className={buttonClass}
     >
       {children}
     </a>
