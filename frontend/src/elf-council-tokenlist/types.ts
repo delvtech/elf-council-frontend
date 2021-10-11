6d46efde03938b80b2f73729ad8ca982cfd554ca
Danny Delott <dannydelott@gmail.com>
2021-10-11 15:16:38 -0700

    Move to elf-council-tokenlist sub directory (#51)

---
+++ frontend/src/elf-council-tokenlist/types.ts
@@ -0,0 +1,49 @@
+/**
+ * Elf Council tokenlist type definitions.
+ */
+import { TokenInfo } from "@uniswap/token-lists";
+
+export type ElementGovernanceTokenInfo = TokenInfo;
+
+export interface CoreVotingContractTokenInfo extends TokenInfo {
+  extensions: {
+    baseQuorum: number;
+    lockDuration: number;
+    minProposalPower: number;
+    extraVoteTime: number;
+  };
+}
+
+export interface ElementLockingVaultTokenInfo extends TokenInfo {
+  extensions: {
+    token: string;
+    staleBlockLag: number;
+  };
+}
+
+export interface ElementOptimisticsRewardsVaultTokenInfo extends TokenInfo {
+  extensions: {
+    pendingRoot: string;
+    proposalTime: number;
+    proposer: string;
+    challengePeriod: number;
+    rewardsRoot: string;
+    lockingVault: string;
+    token: string;
+  };
+}
+export interface ElementGSCVaultTokenInfo extends TokenInfo {
+  extensions: {
+    coreVoting: string;
+    votingPowerBound: number;
+    idleDuration: number;
+  };
+}
+
+export type AnyTokenListInfo =
+  | TokenInfo
+  | ElementGovernanceTokenInfo
+  | CoreVotingContractTokenInfo
+  | ElementLockingVaultTokenInfo
+  | ElementOptimisticsRewardsVaultTokenInfo
+  | ElementGSCVaultTokenInfo;
--- frontend/src/elf/contracts.ts
+++ frontend/src/elf/contracts.ts
@@ -1,8 +1,8 @@
 import { ERC20Permit__factory } from "elf-council-typechain";
+import { tokenListJson } from "src/elf-council-tokenlist";
 import { defaultProvider } from "src/elf/providers/providers";
-import tokenList from "src/tokenlist.json";
 
 export const elementTokenContract = ERC20Permit__factory.connect(
-  tokenList.tokens[0].address,
+  tokenListJson.tokens[0].address,
   defaultProvider
 );
