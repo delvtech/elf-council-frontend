2202fbcc17f9f65723b869fdd32c8b0b74430f6c
Danny Delott <dannydelott@gmail.com>
2021-10-13 13:22:04 -0700

    Fix layout on small screens

--- frontend/src/ui/delegate/DelegatePage.tsx
+++ frontend/src/ui/delegate/DelegatePage.tsx
@@ -1,5 +1,5 @@
 import tw from "src/elf-tailwindcss-classnames";
-import React, { ReactElement } from "react";
+import React, { ReactElement, useCallback, useState } from "react";
 import { t } from "ttag";
 import Button from "src/ui/base/Button/Button";
 import OutlinedSection from "src/ui/base/OutlinedSection/OutlinedSection";
@@ -17,16 +17,27 @@ export default function DelegatePage(): ReactElement {
 
   return (
     <div className={tw("flex", "h-full", "pt-8", "px-8")}>
-      <div className={tw("grid", "grid-cols-3", "w-full", "gap-16")}>
-        {/* Left side */}
+      <div
+        className={tw(
+          "w-full",
+          "gap-16",
+          // use flexbox in mobile
+          "flex",
+          "flex-col",
+          // use two column on desktop
+          "lg:grid",
+          "lg:grid-cols-3"
+        )}
+      >
+        {/* Left/Top side */}
         <div className={tw()}>
           <H2
             className={tw("mb-4", "text-brandDarkBlue-dark")}
           >{t`Delegate`}</H2>
           <DelegateSection signer={signer} />
         </div>
-        {/* Right side */}
-        <div className={tw("col-span-2")}>
+        {/* Right/Bottom side */}
+        <div className={tw("lg:col-span-2")}>
           <H2
             className={tw("mb-4", "text-brandDarkBlue-dark")}
           >{t`Delegate Leaderboard`}</H2>
@@ -43,9 +54,15 @@ interface DelegationSectionProps {
 
 function DelegateSection({ signer }: DelegationSectionProps) {
   const { mutate: changeDelegation } = useChangeDelegation(signer);
-  const onClick = useCallback(() => {
-    changeDelegation([]);
-  }, [changeDelegation]);
+  const [delegateAddress, setDelegateAddressInput] = useState<
+    string | undefined
+  >();
+  const onDelegateClick = useCallback(() => {
+    if (delegateAddress) {
+      changeDelegation([delegateAddress]);
+    }
+  }, [changeDelegation, delegateAddress]);
+
   return (
     <OutlinedSection className={tw("space-y-4")}>
       <H3
@@ -65,9 +82,11 @@ function DelegateSection({ signer }: DelegationSectionProps) {
         name={t`Insert Delegate Address`}
         placeholder={t`Insert Delegate Address`}
         className={tw("mb-8", "h-12", "text-center")}
+        value={delegateAddress}
+        onChange={(event) => setDelegateAddressInput(event.target.value)}
       />
       <div className={tw("text-center")}>
-        <Button>{t`Delegate Vote`}</Button>
+        <Button onClick={onDelegateClick}>{t`Delegate Vote`}</Button>
       </div>
     </OutlinedSection>
   );
