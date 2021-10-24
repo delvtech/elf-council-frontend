1ced69ef7b14a85984ef39eaca1e3b4d8e071dad
Matthew Brown <mattfdbrown@gmail.com>
2021-10-24 11:50:59 -0700

    use tw() for LabeledStat

--- frontend/src/ui/base/LabeledStat/LabeledStat.tsx
+++ frontend/src/ui/base/LabeledStat/LabeledStat.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement } from "react";
+import tw from "src/elf-tailwindcss-classnames";
 
 interface LabeledStatProps {
   data: string;
@@ -8,15 +9,21 @@ interface LabeledStatProps {
 export function LabeledStat(props: LabeledStatProps): ReactElement {
   const { data, topLabel, bottomLabel } = props;
   return (
-    <div className="px-4 py-5 overflow-hidden sm:p-6">
+    <div className={tw("px-4", "py-5", "overflow-hidden", "sm:p-6")}>
       {topLabel && (
-        <dt className="text-sm font-medium text-gray-500 truncate">
+        <dt
+          className={tw("text-sm", "font-medium", "text-gray-500", "truncate")}
+        >
           {topLabel}
         </dt>
       )}
-      <dd className="mt-1 text-3xl font-semibold text-gray-900">{data}</dd>
+      <dd className={tw("mt-1", "text-3xl", "font-semibold", "text-gray-900")}>
+        {data}
+      </dd>
       {bottomLabel && (
-        <dt className="text-sm font-medium text-gray-500 truncate">
+        <dt
+          className={tw("text-sm", "font-medium", "text-gray-500", "truncate")}
+        >
           {bottomLabel}
         </dt>
       )}
