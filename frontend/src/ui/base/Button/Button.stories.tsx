881ba2d8f0338b12815ad481f148028840f139d4
Danny Delott <dannydelott@gmail.com>
2021-10-25 16:24:33 -0700

    Add storybook

---
+++ frontend/src/ui/base/Button/Button.stories.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "./Button";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Button",
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  children: <span>Click me</span>,
+};
