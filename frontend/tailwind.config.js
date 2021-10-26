351517fd4fd381b3a39c6540435f97f0b8e408e8
Danny Delott <dannydelott@gmail.com>
2021-10-26 11:16:45 -0700

    Add figma solid colors (#73)

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        // Colors taken from:
+        // https://www.figma.com/file/SRw9lvgMAasN5JaQrK0z4t/Governance?node-id=1040%3A1303
+        yieldBlue: "#1568CA",
+        denimBlue: "#7FBEEA",
+        principalBlue: "#6ACDE2",
+        clay: "#242E36",
+        black: "#1B1D21",
+        blueGrey: "#B2CAE3",
+        paleLily: "#D1ECF1",
+        votingGreen: "#CDFFCD",
+        deepRed: "#F73030",
+        indianYellow: "#E1AF55",
+        topaz: "#19C2A3",
+        goldYellow: "#FFC300",
+        statusGreen: "#007F00",
+
+        // Deprecated colors:
         // Used this tool for color variants: https://material.io/resources/color
         brandLightBlue: {
           light: "#acffff",
