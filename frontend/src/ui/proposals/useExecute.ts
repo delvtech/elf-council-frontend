b69f2ffb6d4d9a84f325a06a7e193d6536619ac9
Matthew Brown <mattfdbrown@gmail.com>
2021-10-27 20:49:00 -0700

    add useExecute hook for proposals (#82)

---
+++ frontend/src/ui/proposals/useExecute.ts
@@ -0,0 +1,38 @@
+import { useCallback } from "react";
+
+import { Signer } from "ethers";
+import { proposalsJson } from "src/elf-council-proposals";
+import { coreVotingContract } from "src/elf/contracts";
+import { useSmartContractTransaction } from "src/react-query-typechain/hooks/useSmartContractTransaction/useSmartContractTransaction";
+
+const { proposals } = proposalsJson;
+
+export function useExecute(signer: Signer | undefined): {
+  mutate: (proposalId: string) => void;
+  isLoading: boolean;
+  isSuccess: boolean;
+  isError: boolean;
+} {
+  const {
+    mutate: execute,
+    isLoading,
+    isSuccess,
+    isError,
+  } = useSmartContractTransaction(coreVotingContract, "execute", signer);
+
+  const onExecute = useCallback(
+    (proposalId: string) => {
+      const proposal = proposals.find((p) => p.proposalId === proposalId);
+
+      if (!proposal) {
+        return;
+      }
+
+      const { targets, calldatas } = proposal;
+      execute([proposalId, targets, calldatas]);
+    },
+    [execute]
+  );
+
+  return { mutate: onExecute, isLoading, isSuccess, isError };
+}
