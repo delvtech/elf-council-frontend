6fe283aa6d29b5d98b158bd17f14f6b3cc6c6507
Matthew Brown <mattfdbrown@gmail.com>
2021-10-29 16:24:01 -0700

    add css

---
+++ frontend/styles/Styles.ts
@@ -0,0 +1,12 @@
+export default TButton;
+
+type TButton =
+  | "btn"
+  | "btn-primary"
+  | "btn-secondary"
+  | "btn-gradient"
+  | "btn-minimal"
+  | "btn-outlined"
+  | "btn-error"
+  | "btn-round"
+  | "btn-disabled";
