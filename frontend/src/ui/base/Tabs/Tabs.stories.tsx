14ba99d7a4ddfbbdb45512fda15e7ba0e4f184e9
Danny Delott <dannydelott@gmail.com>
2021-11-01 17:59:57 -0700

    Add storybook for Tabs component

---
+++ frontend/src/ui/base/Tabs/Tabs.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import TabsComponent from "./Tabs";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Tabs",
+  component: TabsComponent,
+} as ComponentMeta<typeof TabsComponent>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const TabsStory: ComponentStory<typeof TabsComponent> = (args) => (
+  <TabsComponent {...args} />
+);
+
+const onChange = () => {};
+export const Tabs = TabsStory.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Tabs.args = {
+  tabs: [
+    { id: "First", name: "First", current: true, onChange },
+    { id: "Second", name: "Second", current: false, onChange },
+    { id: "Third", name: "Third", current: false, onChange },
+  ],
+  "aria-label": "Storybook tabs label",
+};
