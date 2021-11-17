1dcbcfe83a6a75a2cf88ff668f2283f70e019538
Matthew Brown <mattfdbrown@gmail.com>
2021-11-16 18:03:19 -0700

    fetch merkle info from s3

--- src/elf/merkle/useMerkleInfo.ts
+++ src/elf/merkle/useMerkleInfo.ts
@@ -1,22 +1,55 @@
 import { QueryObserverResult, useQuery } from "react-query";
 
+import { BigNumber } from "ethers";
+import { formatEther } from "ethers/lib/utils";
+import { addressesJson } from "src/elf-council-addresses";
 import { MerkleProof } from "src/elf/merkle/MerkleProof";
+import { ChainId } from "src/ethereum";
 
 const merkleServerUrl = process.env.NEXT_PUBLIC_MERKLE_SERVER_URL;
+const ELEMENT_REWARDS_URL =
+  "https://elementfi.s3.us-east-2.amazonaws.com/rewards";
+
+const { chainId } = addressesJson;
+const chainName = process.env.NEXT_PUBLIC_CHAIN_NAME || "testnet";
+
+enum RewardType {
+  RETRO = "retro",
+  REWARDS_VAULT = "rewardsVault",
+}
 
 export function useMerkleInfo(
-  address: string | undefined | null
+  address: string | undefined | null,
+  rewardType: RewardType = RewardType.RETRO
 ): QueryObserverResult<MerkleProof> {
   return useQuery({
     queryKey: ["merkleInfo", address],
-    queryFn: () => fetchMerkleInfo(address as string),
+    queryFn: () => fetchMerkleInfo(address as string, rewardType),
     enabled: !!address,
   });
 }
 
-export async function fetchMerkleInfo(address: string): Promise<MerkleProof> {
-  const result = await fetch(`${merkleServerUrl}/api/merkleProof/${address}`);
+export async function fetchMerkleInfo(
+  address: string,
+  rewardType: RewardType
+): Promise<MerkleProof> {
+  // TODO: host this on s3 as well
+  if (chainId === ChainId.LOCAL) {
+    const result = await fetch(`${merkleServerUrl}/api/merkleProof/${address}`);
+    const merkleProof = (await result.json()) as MerkleProof;
+    return merkleProof;
+  }
 
-  const merkleProof = (await result.json()) as MerkleProof;
+  const result = await fetch(
+    `${ELEMENT_REWARDS_URL}/${chainName}/${rewardType}/${address}`
+  );
+
+  const merkleProofWithBN = (await result.json())[0] as MerkleProof;
+  const value = BigNumber.from(merkleProofWithBN.leaf.value);
+
+  const merkleProof = {
+    ...merkleProofWithBN,
+    leaf: { address, value: formatEther(value) },
+  };
   return merkleProof;
 }
