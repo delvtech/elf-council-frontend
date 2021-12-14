8c5ee82ec7ddfad2b5c26b1e347293a13a63b589
Matthew Brown <mattfdbrown@gmail.com>
2021-12-14 13:23:14 -0300

    add non fung to voting hooks

--- src/ui/voting/useVotingPowerForAccount.ts
+++ src/ui/voting/useVotingPowerForAccount.ts
@@ -1,3 +1,4 @@
+import { rewardsContract, nonFungibleVotingContract } from "src/elf/contracts";
 import { FixedNumber } from "ethers";
 import { useLockingVaultVotingPower } from "src/ui/voting/useLockingVaultVotingPower";
 import { useRewardsVaultVotingPower } from "src/ui/voting/useRewardsVaultVotingPower";
@@ -9,7 +10,16 @@ export function useVotingPowerForAccount(
   // The CoreVoting contract passes this parameter to the rewards contract when querying vote power,
   // so we have to pass it as well, though the rewards contract doesn't need this to calculate vote
   // power.
-  const rewardsVotingPower = useRewardsVaultVotingPower(account, atBlockNumber);
+  const rewardsVotingPower = useRewardsVaultVotingPower(
+    account,
+    rewardsContract,
+    atBlockNumber
+  );
+  const nonFungibleVotingPower = useRewardsVaultVotingPower(
+    account,
+    nonFungibleVotingContract,
+    atBlockNumber
+  );
 
   const lockingVotingPower = useLockingVaultVotingPower(account, atBlockNumber);
 
