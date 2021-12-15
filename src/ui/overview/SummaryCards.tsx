a6d8a88540e6d8892134ef9203d2d7ab2bee8b92
Matthew Brown <mattfdbrown@gmail.com>
2021-12-15 19:07:59 -0300

    Show treasury balance (#136)

--- src/ui/overview/SummaryCards.tsx
+++ src/ui/overview/SummaryCards.tsx
@@ -1,13 +1,26 @@
 import React, { ReactElement } from "react";
 
-import { commify } from "ethers/lib/utils";
+import { commify, formatEther } from "ethers/lib/utils";
 import tw from "src/elf-tailwindcss-classnames";
 import SummaryCard from "src/ui/overview/SummaryCard";
 import { useVotingPowerForProtocol } from "src/ui/voting/useVotingPowerForProtocol";
 import { t } from "ttag";
+import { useTokenBalanceOf } from "src/elf/token/useTokenBalanceOf";
+import { addressesJson } from "src/elf-council-addresses";
+import { elementTokenContract } from "src/elf/contracts";
+import { abbreviateLargeValue } from "src/base/numbers";
+
+const { treasury } = addressesJson.addresses;
 
 export function SummaryCards(): ReactElement {
   const votingPower = useVotingPowerForProtocol();
+  const { data: treasuryBalanceBN } = useTokenBalanceOf(
+    elementTokenContract,
+    treasury
+  );
+
+  const treasuryBalance = formatEther(treasuryBalanceBN || 0);
+  const formattedTreasuryBalance = abbreviateLargeValue(+treasuryBalance);
 
   return (
     <div
@@ -22,7 +35,10 @@ export function SummaryCards(): ReactElement {
       )}
     >
       <SummaryCard title={t`Annual protocol revenue`} balance={"$180,000"} />
-      <SummaryCard title={t`Total treasury`} balance={"5M ELFI"} />
+      <SummaryCard
+        title={t`Total treasury`}
+        balance={`${formattedTreasuryBalance} ELFI`}
+      />
       <SummaryCard
         title={t`Total Voting Power`}
         balance={commify(votingPower)}
