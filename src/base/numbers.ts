029f2240be2e50b17d81cfd0561e570dce475018
Matthew Brown <mattfdbrown@gmail.com>
2021-12-15 18:28:16 -0300

    display formatted balance

--- src/base/numbers.ts
+++ src/base/numbers.ts
@@ -1,3 +1,5 @@
+import { format } from "d3-format";
+import { commify } from "ethers/lib/utils";
 export const ANY_NUMBER_REGEX = /^-?[0-9]\d*\.?\d*$/;
 
 /**
@@ -14,4 +16,15 @@ export function isFiniteNumber(num: unknown): num is number {
  */
 export function isIntegerNumber(num: unknown): num is number {
   return Number.isInteger(num as number);
+}
+
+export function abbreviateLargeValue(value: number): string {
+  const f = format(".2s");
+
+  if (value > 10000) {
+    // use 'B' for billion, not 'G' for giga
+    return f(value).replace("G", "B");
+  }
+
+  return commify(value);
 }
