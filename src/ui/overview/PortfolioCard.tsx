a498c45a94f9071f0520f58531b9f1e6521e4a5b
Matthew Brown <mattfdbrown@gmail.com>
2021-12-18 15:50:15 -0300

    asdf

--- src/ui/overview/PortfolioCard.tsx
+++ src/ui/overview/PortfolioCard.tsx
@@ -1,20 +1,20 @@
 import React, { ReactElement } from "react";
 
+import classNames from "classnames";
 import { formatEther } from "ethers/lib/utils";
 import tw from "src/elf-tailwindcss-classnames";
 import { elementTokenContract } from "src/elf/contracts";
 import { useMerkleInfo } from "src/elf/merkle/useMerkleInfo";
 import { useTokenBalanceOf } from "src/elf/token/useTokenBalanceOf";
-import Button from "src/ui/base/Button/Button";
+import { formatWalletAddress } from "src/formatWalletAddress";
+import { useUnclaimedAirdrop } from "src/ui/airdrop/useUnclaimedAirdrop";
+import LinkButton from "src/ui/base/Button/LinkButton";
 import { ButtonVariant } from "src/ui/base/Button/styles";
 import Card, { CardVariant } from "src/ui/base/Card/Card";
-import { LabeledStat } from "src/ui/base/LabeledStat/LabeledStat";
+import { ElementIcon, IconSize } from "src/ui/base/ElementIcon";
 import { useDeposited } from "src/ui/base/lockingVault/useDeposited";
-import { useUnclaimed } from "src/ui/rewards/useUnclaimed";
-import { t } from "ttag";
 import { useVotingPowerForAccount } from "src/ui/voting/useVotingPowerForAccount";
-import AnchorButton from "src/ui/base/Button/AnchorButton";
-import LinkButton from "src/ui/base/Button/LinkButton";
+import { t } from "ttag";
 
 interface PortfolioCardProps {
   account: string | undefined | null;
@@ -28,24 +28,66 @@ export function PortfolioCard(props: PortfolioCardProps): ReactElement {
   const amountDeposited = useDeposited(account) || "0";
 
   const { data: merkleInfo } = useMerkleInfo(account);
-  const unclaimed = useUnclaimed(account, merkleInfo);
+  const unclaimedAirdrop = useUnclaimedAirdrop(account, merkleInfo);
   const votingPower = useVotingPowerForAccount(account);
 
   return (
-    <Card variant={CardVariant.BLUE}>
-      <div className={tw("text-white", "font-light")}>{t`Portfolio`}</div>
-      <div className={tw("flex", "flex-col", "min-h-full", "align-bottom")}>
-        <LabeledStat whiteText data={balance} topLabel={t`Wallet balance`} />
-        <LabeledStat
-          whiteText
-          data={amountDeposited}
-          topLabel={t`Voting vault balance`}
-        />
+    <Card variant={CardVariant.GRADIENT} className={tw("shadow-md")}>
+      <div>
+        <span
+          className={tw(
+            "text-white",
+            "font-bold",
+            "text-xl",
+            "tracking-widest"
+          )}
+        >{t`Portfolio`}</span>
+        <span
+          className={tw(
+            "ml-2",
+            "text-white",
+            "font-light",
+            "text-sm",
+            "tracking-widest"
+          )}
+        >
+          {account && `(${formatWalletAddress(account)})`}
+        </span>
+      </div>
+      <div
+        className={tw("flex", "flex-col", "min-h-full", "align-bottom", "mb-8")}
+      >
         <div className={tw("flex", "items-center", "justify-between")}>
-          <LabeledStat
-            whiteText
-            data={unclaimed}
-            topLabel={t`Unclaimed rewards`}
+          <BalanceWithLabel
+            className={tw("mt-8")}
+            balance={balance}
+            label={t`Wallet balance`}
+          />
+          <BalanceWithLabel
+            className={tw("mt-8")}
+            balance={unclaimedAirdrop}
+            label={t`Voting Power`}
+          />
+        </div>
+        <BalanceWithLabel
+          className={tw("mt-8")}
+          balance={amountDeposited}
+          label={t`Eligible voting balance`}
+        />
+        <div
+          className={tw(
+            "flex",
+            "items-center",
+            "justify-between",
+            "border-t",
+            "border-white",
+            "mt-4",
+            "pt-4"
+          )}
+        >
+          <BalanceWithLabel
+            balance={unclaimedAirdrop}
+            label={t`Unclaimed airdrop`}
           />
           <LinkButton
             link="/airdrop"
@@ -53,8 +95,26 @@ export function PortfolioCard(props: PortfolioCardProps): ReactElement {
             variant={ButtonVariant.OUTLINE_WHITE}
           >{t`Claim`}</LinkButton>
         </div>
-        <LabeledStat whiteText data={votingPower} topLabel={t`Voting Power`} />
       </div>
     </Card>
+  );
+}
+
+interface BalanceWithLabelProps {
+  balance: string;
+  label: string;
+  className?: string;
+}
+
+function BalanceWithLabel(props: BalanceWithLabelProps) {
+  const { className, balance, label } = props;
+  return (
+    <div className={classNames(className, tw("text-white"))}>
+      <div className={tw("flex", "items-center")}>
+        <div className={tw("text-2xl", "font-extralight")}>{balance}</div>
+        <ElementIcon className={tw("ml-2")} size={IconSize.MEDIUM} />
+      </div>
+      <div className={tw("flex", "text-lg", "font-light")}>{label}</div>
+    </div>
   );
 }
