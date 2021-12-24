3a529178f253c4183c812460cd2c78bf374fd9be
Duke <hnguyen1179@berkeley.edu>
2021-12-23 19:46:54 -0800

    Add basic detailed delegate profile

--- src/ui/delegate/DelegatesList/DelegatesList.tsx
+++ src/ui/delegate/DelegatesList/DelegatesList.tsx
@@ -1,10 +1,11 @@
-import { ReactElement } from "react";
+import { ReactElement, useState, Fragment } from "react";
 import { delegates } from "src/elf-council-delegates/delegates";
 import tw, {
   display,
   gridTemplateColumns,
   gap,
   overflow,
+  maxHeight,
   margin,
   fontSize,
   textColor,
@@ -15,6 +16,12 @@ import H2 from "src/ui/base/H2";
 import { t } from "ttag";
 
 function DelegatesList(): ReactElement {
+  const [currentDelegate, setCurrentDelegate] = useState(-1);
+
+  const onClose = () => {
+    setCurrentDelegate(-1);
+  };
+
   return (
     <div className="relative">
       <H2
@@ -32,17 +39,37 @@ function DelegatesList(): ReactElement {
           gridTemplateColumns("grid-cols-2", "lg:grid-cols-3"),
           gap("gap-x-4", "gap-y-3"),
           overflow("overflow-y-scroll"),
+          maxHeight("max-h-[428px]"),
         )}
-        style={{ maxHeight: "428px" }}
       >
-        {delegates.map((delegate) => {
+        {delegates.map((delegate, idx) => {
           return (
-            <li key={delegate.address}>
-              <DelegateProfile delegate={delegate} />
+            <li key={`${delegate.address}-${idx}}`}>
+              <button onClick={() => setCurrentDelegate(idx)}>
+                <DelegateProfile delegate={delegate} />
+              </button>
             </li>
           );
         })}
       </ul>
+
+      {/* Detailed delegate profile */}
+      {currentDelegate !== -1 ? (
+        <Fragment>
+          <div className="absolute z-50 box-content py-2 px-3 rounded-xl top-0 
+          right-0 h-full w-full bg-hackerSky">
+            <div className="m-12">
+              <button onClick={onClose}>CLOSE DELEGATE</button>
+
+              {delegates[currentDelegate].name}
+            </div>
+          </div>
+          <div
+            onClick={onClose}
+            className="fixed z-30 inset-0 bg-black opacity-50"
+          />
+        </Fragment>
+      ) : null}
     </div>
   );
 }
