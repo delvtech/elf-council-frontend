8901f5a1230ca33b4ab1f769e94d8f6b366d82ff
Duke Nguyen <hnguyen1179@berkeley.edu>
2021-12-25 13:46:47 -0800

    Add approve button to PortfolioCard (#181)

--- src/ui/delegate/PortfolioCard/PortfolioCard.tsx
+++ src/ui/delegate/PortfolioCard/PortfolioCard.tsx
@@ -1,31 +1,21 @@
 import { ReactElement } from "react";
-import { Signer } from "ethers";
+import { ethers, Signer } from "ethers";
 import { parseEther } from "ethers/lib/utils";
 import Link from "next/link";
 import { Delegate } from "src/elf-council-delegates/delegates";
 import Button from "src/ui/base/Button/Button";
-import { useNumericInputValue } from "src/ui/base/Input/useNumericInputValue";
-import { BalanceLabeledStat } from "src/ui/delegate/BalanceLabeledStat/BalanceLabeledStat";
+import { ButtonVariant } from "src/ui/base/Button/styles";
 import { DepositInput } from "src/ui/overview/DepositCard/DepositInput";
+import { BalanceLabeledStat } from "src/ui/delegate/BalanceLabeledStat/BalanceLabeledStat";
+import { useNumericInputValue } from "src/ui/base/Input/useNumericInputValue";
+import { useTokenAllowance } from "src/elf/token/useTokenAllowance";
+import { useSetTokenAllowance } from "src/ui/base/token/useSetTokenAllowance";
 import { useWithdrawFromLockingVault } from "src/ui/rewards/useWithdrawFromLockingVault";
 import { useDepositIntoLockingVault } from "src/ui/rewards/useDepositIntoLockingVault";
 import { jt, t } from "ttag";
-import tw, {
-  display,
-  margin,
-  width,
-  justifyContent,
-  gap,
-  flexDirection,
-  textColor,
-  fontSize,
-  fontWeight,
-  lineHeight,
-  opacity,
-  textAlign,
-  cursor,
-} from "src/elf-tailwindcss-classnames";
-import { ButtonVariant } from "src/ui/base/Button/styles";
+import classNames from "classnames";
+import { addressesJson } from "src/elf-council-addresses";
+import { elementTokenContract } from "src/elf/contracts";
 
 interface PortfolioCardProps {
   account: string | null | undefined;
@@ -38,53 +28,64 @@ interface PortfolioCardProps {
 const portfolioTooltip = t`Don’t know what the difference between your wallet balance and eligible voting balance is? Click this icon to learn more`;
 
 function PortfolioCard(props: PortfolioCardProps): ReactElement {
+  const { elementToken, lockingVault } = addressesJson.addresses;
   const { account, signer, currentDelegate, walletBalance, vaultBalance } =
     props;
 
-  const { value: deposit, setNumericValue: setDeposit } =
+  const { value: depositAmount, setNumericValue: setDepositAmount } =
     useNumericInputValue();
-  const { value: withdraw, setNumericValue: setWithdraw } =
+  const { value: withdrawAmount, setNumericValue: setWithdrawAmount } =
     useNumericInputValue();
 
-  const clearDepositInput = () => setDeposit("");
-  const clearWithdrawInput = () => setWithdraw("");
+  const clearDepositInput = () => setDepositAmount("");
+  const clearWithdrawInput = () => setWithdrawAmount("");
 
-  const { mutate: onDeposit } = useDepositIntoLockingVault(
+  const { mutate: deposit } = useDepositIntoLockingVault(
     signer,
     clearDepositInput,
   );
 
-  const { mutate: onWithdraw } = useWithdrawFromLockingVault(
+  const { mutate: withdraw } = useWithdrawFromLockingVault(
     signer,
     clearWithdrawInput,
   );
 
-  const depositClickHandler = () => {
-    if (!account || !signer || !currentDelegate) return;
-    onDeposit([account, parseEther(deposit), currentDelegate.address]);
+  const { mutate: allow } = useSetTokenAllowance(signer, elementToken);
+
+  const { data: allowanceBN } = useTokenAllowance(
+    elementTokenContract,
+    account,
+    lockingVault,
+  );
+
+  const isAllowed = allowanceBN?.gt(parseEther(depositAmount || "0")) || false;
+
+  const onSetAllowance = () => {
+    if (!account || !signer || !lockingVault) return;
+
+    allow([lockingVault, ethers.constants.MaxUint256]);
   };
 
-  const withdrawClickHandler = () => {
+  const onDeposit = () => {
+    if (!account || !signer || !currentDelegate) return;
+    deposit([account, parseEther(depositAmount), currentDelegate.address]);
+  };
+
+  const onWithdraw = () => {
     if (!account) return;
-    onWithdraw([parseEther(withdraw)]);
+    withdraw([parseEther(withdrawAmount)]);
   };
 
   return (
-    <div className={tw(opacity({ "opacity-50": !account }))}>
+    <div className={classNames({ "opacity-50": !account })}>
       {/* Balance Stats */}
-      <div
-        className={tw(
-          display("flex"),
-          flexDirection("flex-col"),
-          margin("mb-4"),
-        )}
-      >
+      <div className="flex flex-col mb-4">
         <BalanceLabeledStat
           tooltip={portfolioTooltip}
           tooltipHref="/resources"
           label={t`Wallet Balance`}
           balance={walletBalance}
-          className={margin("mb-2")}
+          className="mb-2"
         />
         <BalanceLabeledStat
           tooltip={portfolioTooltip}
@@ -97,77 +98,64 @@ function PortfolioCard(props: PortfolioCardProps): ReactElement {
       {/* Deposit Section */}
       <div>
         <PortfolioDepositText />
-        <div className={margin("mt-3")}>
-          <div
-            className={tw(
-              textColor("text-white"),
-              fontSize("text-sm"),
-              margin("mb-2"),
-            )}
-          >{jt`Tokens Eligible to Deposit: ${walletBalance}`}</div>
+        <div className="mt-3">
+          <div className="text-white text-sm mb-2">
+            {jt`Tokens Eligible to Deposit: ${walletBalance}`}
+          </div>
           <DepositInput
-            depositAmount={deposit}
+            depositAmount={depositAmount}
             balance={walletBalance}
-            onDeposit={setDeposit}
+            onDeposit={setDepositAmount}
             id={"deposit-amount"}
             name={t`Deposit amount`}
             placeholder={t`Insert amount to deposit`}
             screenReaderLabel={t`Amount to deposit`}
           />
         </div>
-        <div
-          className={tw(
-            width("w-full"),
-            display("flex"),
-            justifyContent("justify-end"),
-            margin("mt-4"),
-            gap("gap-4"),
-          )}
-        >
+        <div className="w-full flex justify-end mt-4 gap-4">
+          {!isAllowed ? (
+            <Button
+              onClick={onSetAllowance}
+              disabled={!account}
+              variant={ButtonVariant.GRADIENT}
+              className="w-28 justify-center"
+            >
+              {t`Allow`}
+            </Button>
+          ) : null}
+
           <Button
-            onClick={depositClickHandler}
-            disabled={!parseInt(walletBalance) || !deposit}
+            onClick={onDeposit}
+            disabled={!parseInt(walletBalance) || !depositAmount || !isAllowed}
             variant={ButtonVariant.GRADIENT}
-            className={tw(width("w-28"), justifyContent("justify-center"))}
+            className="w-28 justify-center"
           >{t`Deposit`}</Button>
         </div>
       </div>
 
       {/* Withdraw Section */}
-      <div className={margin("mt-7")}>
+      <div className="mt-7">
         <PortfolioWithdrawText />
-        <div className={margin("mt-3")}>
-          <div
-            className={tw(
-              textColor("text-white"),
-              fontSize("text-sm"),
-              margin("mb-2"),
-            )}
-          >{jt`Tokens Eligible to Withdraw: ${vaultBalance}`}</div>
+        <div className="mt-3">
+          <div className="text-white text-sm mb-2">
+            {jt`Tokens Eligible to Withdraw: ${vaultBalance}`}
+          </div>
           <DepositInput
-            depositAmount={withdraw}
+            depositAmount={withdrawAmount}
             balance={vaultBalance}
-            onDeposit={setWithdraw}
+            onDeposit={setWithdrawAmount}
             id={"withdraw-amount"}
             name={t`Withdraw amount`}
             placeholder={t`Insert amount to withdraw`}
             screenReaderLabel={t`Amount to withdraw`}
           />
         </div>
-        <div
-          className={tw(
-            width("w-full"),
-            display("flex"),
-            justifyContent("justify-end"),
-            margin("mt-4"),
-            gap("gap-4"),
-          )}
-        >
+        <div className="w-full flex justify-end mt-4 gap-4">
           <Button
-            onClick={withdrawClickHandler}
-            disabled={!parseInt(vaultBalance) || !withdraw}
+            onClick={onWithdraw}
+            disabled={!parseInt(vaultBalance) || !withdrawAmount}
             variant={ButtonVariant.WHITE}
-            className={tw(width("w-28"), textAlign("text-center"))}
+            className="w-28 text-center"
           >{t`Withdraw`}</Button>
         </div>
       </div>
@@ -176,31 +164,17 @@ function PortfolioCard(props: PortfolioCardProps): ReactElement {
 }
 
 function PortfolioDepositText(): ReactElement {
-  const deposit = (
-    <span className={tw(fontWeight("font-bold"))}>{t`deposit`}</span>
-  );
+  const deposit = <span className="font-bold">{t`deposit`}</span>;
 
   return (
-    <p
-      className={tw(
-        textColor("text-white"),
-        fontWeight("font-light"),
-        lineHeight("leading-5"),
-        fontSize("text-sm"),
-      )}
-    >
+    <p className="text-white font-light leading-5 text-sm">
       {jt`To protect our governance system, we ask our users to ${deposit} their tokens when they have the intention to vote and/or delegate.`}{" "}
-      <span className={tw(fontWeight("font-bold"))}>
+      <span className="font-bold">
         {t`This verifies your eligibility to vote and/or delegate.`}
       </span>
       <div>
         <Link href="/resources" passHref>
-          <span
-            className={tw(
-              textColor("text-goldYellow"),
-              cursor("cursor-pointer"),
-            )}
-          >
+          <span className="text-goldYellow cursor-pointer">
             {t`To learn more about our vaults read here.`}
           </span>
         </Link>
@@ -211,24 +185,12 @@ function PortfolioDepositText(): ReactElement {
 
 function PortfolioWithdrawText(): ReactElement {
   return (
-    <p
-      className={tw(
-        textColor("text-white"),
-        fontWeight("font-light"),
-        lineHeight("leading-5"),
-        fontSize("text-sm"),
-      )}
-    >
+    <p className="text-white font-light leading-5 text-sm">
       {t`To remove deposited tokens from voting eligibility enter a withdrawal
       amount.`}
       <div>
         <Link href="/resources" passHref>
-          <span
-            className={tw(
-              textColor("text-goldYellow"),
-              cursor("cursor-pointer"),
-            )}
-          >
+          <span className="text-goldYellow cursor-pointer">
             {t`Read more to learn about our voting vaults.`}
           </span>
         </Link>
