0203e87ca6c626af35eb398e713cad170c132472
Matthew Brown <mattfdbrown@gmail.com>
2021-12-27 20:20:04 -0300

    make conversion script

---
+++ src/convertTailwind.ts
@@ -0,0 +1,81 @@
+import { Project, SourceFile } from "ts-morph";
+import * as ts from "typescript";
+
+// example: src/ui/proposals/ProposalList.tsx
+const FILE_PATH = "";
+const project = new Project();
+project.addSourceFilesAtPaths(FILE_PATH);
+const sourceFile = project.getSourceFileOrThrow(FILE_PATH);
+
+const convertTw = (sourceFile: SourceFile) => {
+  /**
+   * converts:
+   *   tw(prop1('a', 'b'), prop2('c', d'))
+   * to:
+   *   'a b c d'
+   */
+  const newSourceFile = sourceFile.transform((traversal) => {
+    const node = traversal.visitChildren(); // Here travseral visits children in postorder
+    if (ts.isCallExpression(node) && node.getText().startsWith("tw")) {
+      let twClasses: string[] = [];
+      node.forEachChild((child) => {
+        if (ts.isCallExpression(child)) {
+          const args = child.arguments
+            .map((a) => {
+              if (ts.isStringLiteral(a)) {
+                return a.text;
+              }
+              return undefined;
+            })
+            .filter((arg): arg is string => !!arg);
+
+          twClasses = [...twClasses, ...args];
+        }
+      });
+
+      return ts.factory.createStringLiteral(twClasses.join(" "));
+    }
+
+    return node;
+  });
+
+  /**
+   * converts:
+   *   className={'flex flex-col'}
+   * to:
+   *   className='flex flex-col'
+   */
+  return (
+    newSourceFile
+      .transform((traversal) => {
+        const node = traversal.visitChildren(); // Here travseral visits children in postorder
+        if (
+          ts.isJsxExpression(node) &&
+          ts.isJsxAttribute(node.parent) &&
+          node.parent.name.escapedText === "className"
+        ) {
+          if (
+            node.getChildCount() === 3 &&
+            node
+              .getChildren()
+              .every((child) =>
+                [
+                  ts.SyntaxKind.OpenBraceToken,
+                  ts.SyntaxKind.StringLiteral,
+                  ts.SyntaxKind.CloseBraceToken,
+                ].includes(child.kind),
+              )
+          ) {
+            return node
+              .getChildren()
+              .filter((child) => ts.isStringLiteral(child))[0];
+          }
+        }
+        return node;
+      })
+      // remove unused imports
+      .fixUnusedIdentifiers()
+  );
+};
+
+convertTw(sourceFile).saveSync();
