b33ac6423a53b18996529dcf23fb495bc81c8ed9
Matthew Brown <mattfdbrown@gmail.com>
2021-12-28 08:07:22 -0300

    remove tailwindcss-classnames

--- src/ui/overview/OverviewPage.tsx
+++ src/ui/overview/OverviewPage.tsx
@@ -2,14 +2,6 @@ import React, { ReactElement } from "react";
 
 import { Web3Provider } from "@ethersproject/providers";
 import { useWeb3React } from "@web3-react/core";
-import tw, {
-  height,
-  space,
-  padding,
-  textAlign,
-  display,
-  gridTemplateColumns,
-} from "src/elf-tailwindcss-classnames";
 import H1 from "src/ui/base/H1";
 import { PortfolioCard } from "src/ui/overview/PortfolioCard";
 import { t } from "ttag";
@@ -19,21 +11,12 @@ import { SummaryCards } from "./SummaryCards";
 export function OverviewPage(): ReactElement {
   const { account } = useWeb3React<Web3Provider>();
   return (
-    <div className={tw(height("h-full"), space("space-y-6"))}>
-      <div className={padding("px-8", "py-1")}>
-        <H1 className={textAlign("text-center")}>
-          {" "}
-          {t`Governance Overview`}
-        </H1>
+    <div className="h-full space-y-6">
+      <div className="px-8 py-1">
+        <H1 className="text-center"> {t`Governance Overview`}</H1>
       </div>
       <SummaryCards />
-      <div
-        className={tw(
-          display("grid"),
-          gridTemplateColumns("grid-cols-1", "lg:grid-cols-2"),
-          space("space-y-6", "space-x-0", "lg:space-x-6", "lg:space-y-0"),
-        )}
-      >
+      <div className="grid grid-cols-1 space-x-0 space-y-6 lg:grid-cols-2 lg:space-x-6 lg:space-y-0">
         <PortfolioCard account={account} />
       </div>
     </div>
