61c135c45610b367425187eba2d880d6c6dac8c9
Danny Delott <dannydelott@gmail.com>
2022-01-03 14:12:34 -0800

    convert-tw

--- src/ui/airdrop/AirdropAlreadyClaimed/ClaimedAmountCard.tsx
+++ src/ui/airdrop/AirdropAlreadyClaimed/ClaimedAmountCard.tsx
@@ -1,16 +1,4 @@
 import React, { ReactElement } from "react";
-import tw, {
-  display,
-  flexDirection,
-  textColor,
-  height,
-  alignItems,
-  fontSize,
-  margin,
-  fontWeight,
-  width,
-  flex,
-} from "src/elf-tailwindcss-classnames";
 import Card, { CardVariant } from "src/ui/base/Card/Card";
 import { t } from "ttag";
 import { ElementIcon, IconSize } from "src/ui/base/ElementIcon";
@@ -24,48 +12,18 @@ export function ClaimedAmountCard({
 }: ClaimedAmountCardProps): ReactElement {
   const claimedAmount = useClaimedAirdrop(account);
   return (
-    <Card
-      variant={CardVariant.HACKER_SKY}
-      className={tw(flex("flex-1"), height("h-64"))}
-    >
-      <div
-        className={tw(
-          height("h-full"),
-          width("w-full"),
-          display("flex"),
-          flexDirection("flex-col"),
-        )}
-      >
-        <div
-          className={tw(
-            fontSize("text-lg"),
-            fontWeight("font-bold"),
-            textColor("text-gray-500"),
-            margin("mb-2"),
-          )}
-        >{t`You deposited`}</div>
-        <div className={tw(flex("flex-1"))}>
-          <div
-            className={tw(
-              textColor("text-principalRoyalBlue"),
-              fontSize("text-2xl"),
-              fontWeight("font-bold"),
-            )}
-          >
+    <Card variant={CardVariant.HACKER_SKY} className="flex-1 h-64">
+      <div className="h-full w-full flex flex-col">
+        <div className="text-lg font-bold text-gray-500 mb-2">{t`You claimed and deposited`}</div>
+        <div className="flex-1">
+          <div className="text-principalRoyalBlue text-2xl font-bold">
             {claimedAmount}
           </div>
-          <div
-            className={tw(
-              textColor("text-gray-500"),
-              display("flex"),
-              flexDirection("flex-col"),
-              alignItems("items-center"),
-            )}
-          >
-            <span className={tw(margin("mb-4"))}>{t`$ELFI tokens`}</span>
+          <div className="text-gray-500 flex flex-col items-center">
+            <span className="mb-4">{t`$ELFI tokens`}</span>
             <ElementIcon
               bgColorClassName="bg-paleLily"
-              className={tw(margin("ml-2"))}
+              className="ml-2"
               size={IconSize.LARGE}
             />
           </div>
