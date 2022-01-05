10b2136d77567423d3c5a72f6159ed56f4d96d4b
Duke Nguyen <hnguyen1179@berkeley.edu>
2022-01-04 18:56:07 -0800

    Add accessibility & transition to DetailedDelegateProfile (#254)
    * Add accessibility & transition to detailed delegate profile
    
    * Clean up comments
    
    * Refactor css & remove unused imports
    
    * Refactor DelegateProfile to contain all logic for popover
    
    * Convert 'Choose Delegate' button to be optional
    
    * Edit handler naming

--- src/ui/delegate/DelegatesList/DelegatesList.tsx
+++ src/ui/delegate/DelegatesList/DelegatesList.tsx
@@ -1,19 +1,8 @@
-import { ReactElement, useState } from "react";
-import { delegates } from "src/elf-council-delegates/delegates";
-import tw, {
-  display,
-  gridTemplateColumns,
-  gap,
-  overflow,
-  margin,
-  fontSize,
-  textColor,
-  letterSpacing,
-} from "src/elf-tailwindcss-classnames";
-import DelegateProfile from "src/ui/delegate/DelegatesList/DelegateProfile";
-import H2 from "src/ui/base/H2";
+import { ReactElement } from "react";
 import { t } from "ttag";
-import DetailedDelegateProfile from "./DetailedDelegateProfile";
+import H2 from "src/ui/base/H2";
+import DelegateProfile from "src/ui/delegate/DelegatesList/DelegateProfile";
+import { delegates } from "src/elf-council-delegates/delegates";
 
 interface DelegatesListProps {
   selectedDelegate: string;
@@ -24,31 +13,13 @@ function DelegatesList({
   selectedDelegate,
   setDelegateAddressInput,
 }: DelegatesListProps): ReactElement {
-  const [currentDelegate, setCurrentDelegate] = useState(-1);
-
-  const onClose = () => {
-    setCurrentDelegate(-1);
-  };
-
   return (
     <div className="relative mb-8">
-      <H2
-        className={tw(
-          margin("mb-4"),
-          fontSize("text-2xl"),
-          textColor("text-principalRoyalBlue"),
-          letterSpacing("tracking-wide"),
-        )}
-      >{t`Explore Featured Delegates`}</H2>
+      <H2 className="mb-4 text-2xl text-principalRoyalBlue tracking-wide">{t`Explore Featured Delegates`}</H2>
 
       {/* List of delegates */}
       <ul
-        className={tw(
-          display("grid"),
-          gridTemplateColumns("grid-cols-2", "lg:grid-cols-3"),
-          gap("gap-x-4", "gap-y-3"),
-          overflow("overflow-y-scroll"),
-        )}
+        className="grid grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-3 overflow-y-scroll"
         // 428px exactly matches 5 rows of the list
         style={{ maxHeight: "428px" }}
       >
@@ -58,25 +29,15 @@ function DelegatesList({
           return (
             // TODO: Remove -${idx} for production since addresses are always unique
             <li key={`${delegate.address}-${idx}}`}>
-              <button
-                className="w-full"
-                onClick={() => setCurrentDelegate(idx)}
-              >
-                <DelegateProfile selected={selected} delegate={delegate} />
-              </button>
+              <DelegateProfile
+                selected={selected}
+                delegate={delegate}
+                onChooseDelegate={setDelegateAddressInput}
+              />
             </li>
           );
         })}
       </ul>
-
-      {/* Detailed delegate profile */}
-      {currentDelegate !== -1 ? (
-        <DetailedDelegateProfile
-          delegate={delegates[currentDelegate]}
-          onClose={onClose}
-          setDelegateAddressInput={setDelegateAddressInput}
-        />
-      ) : null}
     </div>
   );
 }
