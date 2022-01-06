d6df871843d3783cea20bfcbcca0e16b9ad88618
Matthew Brown <mattfdbrown@gmail.com>
2022-01-06 07:57:58 -0300

    add proposals json directly to frontend

--- src/elf-council-proposals/index.ts
+++ src/elf-council-proposals/index.ts
@@ -1,8 +1,10 @@
 import { Proposal, testnetProposals } from "elf-council-proposals";
 import keyBy from "lodash.keyby";
 
+import localTestnetProposals from "./testnet.proposals.json";
 // TODO: Choose the right proposalsJson based on environment
-export const proposalsJson = testnetProposals;
+export const proposalsJsonOriginal = testnetProposals;
+export const proposalsJson = localTestnetProposals;
 
 export const proposalsBySnapShotId = keyBy(
   proposalsJson.proposals,
