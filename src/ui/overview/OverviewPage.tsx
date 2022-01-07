18f1c996f99e9f260c50ab65d01269e5f6bfc42a
Matthew Brown <mattfdbrown@gmail.com>
2022-01-06 22:31:22 -0300

    copy example FAQ from headless

--- src/ui/overview/OverviewPage.tsx
+++ src/ui/overview/OverviewPage.tsx
@@ -1,10 +1,13 @@
 import React, { ReactElement } from "react";
 
 import { Web3Provider } from "@ethersproject/providers";
+import { Disclosure } from "@headlessui/react";
+import { ChevronUpIcon } from "@heroicons/react/solid";
 import { useWeb3React } from "@web3-react/core";
+import { t } from "ttag";
+
 import H1 from "src/ui/base/H1";
 import { PortfolioCard } from "src/ui/overview/PortfolioCard";
-import { t } from "ttag";
 
 import { SummaryCards } from "./SummaryCards";
 
@@ -15,12 +18,57 @@ export function OverviewPage(): ReactElement {
       <div className="px-8 py-1">
         <H1 className="text-center"> {t`Governance Overview`}</H1>
       </div>
+      <SummaryCards />
       <div className="flex flex-col justify-center w-full space-y-6 lg:space-x-6 lg:flex-row lg:space-y-0">
-        <SummaryCards />
         <PortfolioCard account={account} />
+        <FAQ />
       </div>
     </div>
   );
 }
 
 export default OverviewPage;
+
+function FAQ() {
+  return (
+    <div className="w-full px-4 pt-16">
+      <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>What is your refund policy?</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                {t`If you're unhappy with your purchase for any reason, email us
+                within 90 days and we'll refund you in full, no questions asked.`}
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+        <Disclosure as="div" className="mt-2">
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>Do you offer technical support?</span>
+                <ChevronUpIcon
+                  className={`${
+                    open ? "transform rotate-180" : ""
+                  } w-5 h-5 text-purple-500`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
+                No.
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
+  );
+}
