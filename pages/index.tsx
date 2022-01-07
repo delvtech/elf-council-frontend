2035bb63039fbfa271908a30daff0a5bcedc865e
Danny Delott <dannydelott@gmail.com>
2022-01-06 16:46:25 -0800

    Remove from ssr, nothing in OverviewPage tree uses proposals.json

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import OverviewPage from "src/ui/overview/OverviewPage";
 import React, { ReactElement } from "react";
 import PageView from "src/ui/app/PageView";
-import { ProposalsJson } from "elf-council-proposals";
 
 export default function Home(): ReactElement {
   return (
@@ -9,14 +8,4 @@ export default function Home(): ReactElement {
       <OverviewPage />
     </PageView>
   );
-}
-
-export async function getServerSideProps(): Promise<{
-  props: { proposalsJson: ProposalsJson };
-}> {
-  const res = await fetch(
-    "https://elementfi.s3.us-east-2.amazonaws.com/testnet.proposals.json",
-  );
-  const proposalsJson = await res.json();
-  return { props: { proposalsJson } };
 }
