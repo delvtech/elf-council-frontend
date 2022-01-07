2f7101a716a8efa5f5bc4ef7177632ad93af0dca
Danny Delott <dannydelott@gmail.com>
2022-01-06 17:06:34 -0800

    Use correct s3 proposals json depending on chain id

--- src/elf-council-proposals/index.ts
+++ src/elf-council-proposals/index.ts
@@ -1,8 +1,20 @@
 import { Proposal } from "elf-council-proposals";
 import keyBy from "lodash.keyby";
+import { addressesJson } from "src/elf-council-addresses";
+import { ChainId } from "src/ethereum";
 
-export const PROPOSALS_JSON_URL =
-  "https://elementfi.s3.us-east-2.amazonaws.com/testnet.proposals.json";
+export const PROPOSALS_JSON_URL = getProposalsJsonUrl();
+
+function getProposalsJsonUrl() {
+  switch (addressesJson.chainId) {
+    case ChainId.GOERLI:
+      return "https://elementfi.s3.us-east-2.amazonaws.com/goerli.proposals.json";
+
+    case ChainId.LOCAL:
+    default:
+      return "https://elementfi.s3.us-east-2.amazonaws.com/testnet.proposals.json";
+  }
+}
 
 export function getProposalsBySnapshotId(
   proposals: Proposal[],
