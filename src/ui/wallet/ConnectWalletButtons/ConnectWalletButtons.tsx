41af540fe193d97080e923903d0c19fc2b462b08
Duke <hnguyen1179@berkeley.edu>
2022-01-10 15:11:03 -1000

    Match ConnectWalletButtons to mocks

--- src/ui/wallet/ConnectWalletButtons/ConnectWalletButtons.tsx
+++ src/ui/wallet/ConnectWalletButtons/ConnectWalletButtons.tsx
@@ -18,11 +18,16 @@ interface ConnectWalletButtonsProps {
 export function ConnectWalletButtons({
   onClick,
 }: ConnectWalletButtonsProps): ReactElement {
-  const { activate, deactivate } = useWeb3React<Web3Provider>();
+  const { activate, deactivate, active } = useWeb3React<Web3Provider>();
 
   const deactivateActiveConnector = useCallback(async () => {
     await deactivate();
   }, [deactivate]);
+
+  const onClickClose = async () => {
+    await deactivateActiveConnector();
+    onClick?.();
+  };
 
   const connectToMetaMask = useCallback(async () => {
     await deactivateActiveConnector();
@@ -40,17 +45,18 @@ export function ConnectWalletButtons({
   return (
     <div
       data-testid="connect-wallet-buttons"
-      className="flex flex-col justify-center overflow-auto p-8"
+      className="flex flex-col justify-center overflow-auto p-2"
     >
       <H3 className="text-brandDarkBlue-dark text-center mb-8">{t`Select Wallet`}</H3>
-      <div className="flex gap-6 justify-center">
+      <div className="grid grid-cols-3">
         <Button
-          className="h-auto"
+          size={ButtonSize.AUTO}
           variant={ButtonVariant.MINIMAL}
           onClick={connectToMetaMask}
+          className="grid place-items-center hover:bg-yieldLightBlue hover:bg-opacity-100 hover:text-white shadow-none"
         >
-          <div className="flex flex-col w-32 space-y-2 p-6 justify-center items-center">
-            <div className="relative h-12 w-12">
+          <div className="grid place-items-center">
+            <div className="relative h-20 w-20">
               <Image layout="fill" src="/assets/metamask.svg" alt="MetaMask" />
             </div>
             <span className="text-base">MetaMask</span>
@@ -60,9 +66,10 @@ export function ConnectWalletButtons({
           size={ButtonSize.AUTO}
           variant={ButtonVariant.MINIMAL}
           onClick={connectToWalletConnect}
+          className="grid place-items-center hover:bg-yieldLightBlue hover:bg-opacity-100 hover:text-white shadow-none"
         >
-          <div className="flex flex-col w-32 space-y-2 p-6 justify-center items-center">
-            <div className="relative h-12 w-12">
+          <div className="h-full w-full grid place-items-center">
+            <div className="relative h-20 w-20">
               <Image
                 layout="fill"
                 src="/assets/walletConnectIcon.svg"
@@ -73,6 +80,21 @@ export function ConnectWalletButtons({
           </div>
         </Button>
       </div>
+      <div className="mt-12">
+        <p className="text-center text-principalRoyalBlue">
+          Note: Some connectors can only wallets from their app. Some connectors
+          may also cause a page refresh.
+        </p>
+      </div>
+      {active && onClick ? (
+        <Button
+          variant={ButtonVariant.PALE}
+          onClick={onClickClose}
+          className="grid place-items-center mt-4"
+        >
+          <span>Close Wallet Connection</span>
+        </Button>
+      ) : null}
     </div>
   );
 }
