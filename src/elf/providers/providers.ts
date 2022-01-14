3c0b61266b140f2c31c180b72069d86c05f50747
Matthew Brown <mattfdbrown@gmail.com>
2022-01-14 15:27:53 -0300

    use MockProvider for the test provider

--- src/elf/providers/providers.ts
+++ src/elf/providers/providers.ts
@@ -1,9 +1,10 @@
-import { ExternalProvider, Provider } from "@ethersproject/providers";
-import { providers } from "ethers";
 import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+import { ExternalProvider, Provider } from "@ethersproject/providers";
+import { MockProvider } from "ethereum-waffle";
+import { providers } from "ethers";
 
-import { ChainId } from "src/ethereum";
 import { addressesJson } from "src/elf-council-addresses";
+import { ChainId } from "src/ethereum";
 
 const LOCAL_RPC_HOST = "http://127.0.0.1:8545";
 const ALCHEMY_GOERLI_KEY = process.env.NEXT_PUBLIC_GOERLI_ALCHEMY_KEY as string;
@@ -11,10 +12,10 @@ const ALCHEMY_MAINNET_KEY = process.env
   .NEXT_PUBLIC_MAINNET_ALCHEMY_KEY as string;
 
 const { chainId } = addressesJson;
-// default to mainnet for now
 export const ALCHEMY_GOERLI_HTTP_URL = `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_GOERLI_KEY}`;
 export const ALCHEMY_MAINNET_HTTP_URL = `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_MAINNET_KEY}`;
 
+export const testProvider = new MockProvider();
 // eslint-disable-next-line no-var
 export var defaultProvider = getProvider();
 
@@ -29,8 +30,7 @@ function getProvider() {
       // "development",
     ].includes(process.env.NODE_ENV || "")
   ) {
-    const localhostProvider = new providers.JsonRpcProvider(LOCAL_RPC_HOST);
-    return localhostProvider;
+    return testProvider;
   }
 
   // otherwise, if a chain id is provided, we'll use the corresponding alchemy provider.  right now
