5c897e73f91a163797f5e04791ef8438c6f13a94
Matthew Brown <mattfdbrown@gmail.com>
2022-01-14 22:55:04 -0300

    fix config settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,7 @@
-// import nextJest from "next/jest";
 /*
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-
-// const createJestConfig = nextJest({
-//   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
-//   dir: "./",
-// });
 
 const customJestConfig = {
   // All imported modules in your tests should be mocked automatically
@@ -23,7 +17,7 @@ const customJestConfig = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   // collectCoverageFrom: undefined,
@@ -78,14 +72,7 @@ const customJestConfig = {
   moduleDirectories: ["node_modules", "<rootDir>"],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "jsx",
-  //   "ts",
-  //   "tsx",
-  //   "json",
-  //   "node"
-  // ],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -124,9 +111,7 @@ const customJestConfig = {
   // rootDir: undefined,
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  // roots: ["<rootDir>/src"],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -136,6 +121,9 @@ const customJestConfig = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
+    // This will be needed if we upgreade to jest 27.  here's a GH issue that explains it.
+    // https://github.com/prisma/prisma/issues/8558
+    // '<rootDir>/jest.env.js',
     "@testing-library/jest-dom/extend-expect",
     "@testing-library/react",
   ],
@@ -147,7 +135,7 @@ const customJestConfig = {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "node",
+  testEnvironment: "<rootDir>/jest-environment-jsdom.js",
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -182,7 +170,10 @@ const customJestConfig = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+    ".+\\.(css|styl|less|sass|scss)$": "jest-css-modules-transform",
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
@@ -204,4 +195,3 @@ const customJestConfig = {
 };
 
 export default customJestConfig;
-// export default createJestConfig(customJestConfig);
