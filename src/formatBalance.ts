92ef6657a794d6384c65788ff008223a4274a90c
Duke <hnguyen1179@berkeley.edu>
2022-01-18 16:16:32 -0800

    Add more documentation

--- src/formatBalance.ts
+++ src/formatBalance.ts
@@ -4,8 +4,9 @@ import { format } from "d3-format";
 /**
  * Used for final balance presentation since it cuts off decimals
  * @param balance
- * @returns a formatted string with proper commas and 4 decimal places
+ * @param numDecimals max decimals, default is 4
+ * @returns a formatted string with proper commas and {numDecimals} decimal places
  */
-export function formatBalance(balance: string): string {
-  return commify(format(".4~f")(+balance));
+export function formatBalance(balance: string, numDecimals = 4): string {
+  return commify(format(`.${numDecimals}~f`)(+balance));
 }
