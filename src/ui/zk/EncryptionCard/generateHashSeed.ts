a4a2315fa29e543519d3e3149d8ac767182c8620
Ryan Goree <goree.ryan@gmail.com>
2022-01-21 07:07:34 -0600

    seed keys

---
+++ src/ui/zk/EncryptionCard/generateHashSeed.ts
@@ -0,0 +1,17 @@
+export default async function generateHashSeed(): Promise<string | undefined> {
+  if (typeof window !== "undefined") {
+    const key = await window.crypto.subtle.generateKey(
+      {
+        name: "AES-GCM",
+        length: 256,
+      },
+      true,
+      ["encrypt", "decrypt"],
+    );
+    const radix = 16;
+    const buffer = await crypto.subtle.exportKey("raw", key);
+    return Array.from(new Uint8Array(buffer))
+      .map((int) => int.toString(radix).padStart(2, "0"))
+      .join("");
+  }
+}
