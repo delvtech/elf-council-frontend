17cf9f3970e454b3e500532bf1144328481b1938
Ryan Goree <goree.ryan@gmail.com>
2022-01-21 07:29:10 -0600

    add example code from metamask docs

--- src/ui/zkclaim/ClaimCard.tsx
+++ src/ui/zkclaim/ClaimCard.tsx
@@ -1,10 +1,15 @@
-import React, { ReactElement } from "react";
+import React, { ChangeEvent, ReactElement, useState } from "react";
 import Button from "src/ui/base/Button/Button";
 import Card, { CardVariant } from "src/ui/base/Card/Card";
 import { ButtonVariant } from "src/ui/base/Button/styles";
 import H2 from "src/ui/base/H2";
 import ClaimAmountCard from "./ClaimAmountCard";
 import { t } from "ttag";
+
+const TOEKEN_ADDRESS = "0xd00981105e61274c8a5cd5a88fe7e037d935b513";
+const TOKEN_SYMBOL = "ELFI";
+const TOKEN_DECIMALS = 18;
+const TOKEN_IMAGE = "http://placekitten.com/200/300";
 
 interface ClaimCardProps {
   className?: string;
@@ -14,6 +19,37 @@ interface ClaimCardProps {
 const ELFI_TOKEN_AMOUNT = "208.9291341";
 
 export default function ClaimCard({ className }: ClaimCardProps): ReactElement {
+  const [addToWallet, setAddToWallet] = useState(false);
+  const handleAddToWalletChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setAddToWallet(e.target.checked);
+  };
+  const handleClaimClick = async () => {
+    if (addToWallet) {
+      try {
+        // wasAdded is a boolean. Like any RPC method, an error may be thrown.
+        const wasAdded = await window.ethereum.request({
+          method: "wallet_watchAsset",
+          params: {
+            type: "ERC20", // Initially only supports ERC20, but eventually more!
+            options: {
+              address: TOEKEN_ADDRESS, // The address that the token is at.
+              symbol: TOKEN_SYMBOL, // A ticker symbol or shorthand, up to 5 chars.
+              decimals: TOKEN_DECIMALS, // The number of decimals in the token
+              image: TOKEN_IMAGE, // A string url of the token logo
+            },
+          },
+        });
+
+        if (wasAdded) {
+          console.log("Thanks for your interest!");
+        } else {
+          console.log("Your loss!");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
   return (
     <Card className={className} variant={CardVariant.BLUE}>
       <div className="flex flex-col justify-center gap-2 px-8 pt-2 pb-4 text-white sm:pt-6 sm:px-16 md:px-60 sm:pb-14 sm:text-center sm:items-center">
@@ -26,12 +62,15 @@ export default function ClaimCard({ className }: ClaimCardProps): ReactElement {
             type="checkbox"
             className="box-content w-4 h-4 text-current transition-all bg-transparent bg-center border-2 border-current rounded group:hover:border-white hover:border-white checked:border-white checked:focus:border-white checked:hover:border-white checked:text-principalRoyalBlue peer"
             style={{ backgroundSize: "80%" }}
+            checked={addToWallet}
+            onChange={handleAddToWalletChange}
           />
           <span className="transition-all peer-checked:text-white">{t`Add $ELFI to my Metamask`}</span>
         </label>
         <Button
           className="justify-center min-w-full"
           variant={ButtonVariant.GRADIENT}
+          onClick={handleClaimClick}
         >{t`Claim my $ELFI`}</Button>
       </div>
     </Card>
