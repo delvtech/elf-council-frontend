cd8c9e40e32b4870f12825668edff7ff74844b1e
Matthew Brown <mattfdbrown@gmail.com>
2022-01-21 12:25:31 -0300

    rename some files

--- src/test/helpers/renderHookWrapper.tsx
+++ src/test/helpers/renderHookWrapper.tsx
@@ -1,13 +1,20 @@
 import React, { ReactElement, ReactNode } from "react";
 import { QueryClientProvider } from "react-query";
 
+import { Provider } from "@ethersproject/providers";
+
 import { queryClient } from "src/elf/queryClient";
 
 interface WrapperProps {
+  provider: Provider;
   children: ReactNode;
 }
 
-export function queryClientWrapper({ children }: WrapperProps): ReactElement {
+/**
+ * Provides the necessary context to enable testing our hooks.
+ * @returns a wrapped component that renders a hook.
+ */
+export function renderHookWrapper({ children }: WrapperProps): ReactElement {
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
--- src/test/setupFixture.ts
+++ src/test/setupFixture.ts
@@ -2,11 +2,11 @@ import { testProvider } from "src/elf/providers/providers";
 import { deployGovernanace } from "src/test/helpers/deployGovernance";
 import { initializeGovernance } from "src/test/helpers/initializeGovernance";
 
-async function setup(): Promise<void> {
+async function setupFixture(): Promise<void> {
   const wallets = testProvider.getWallets();
   const [owner] = wallets;
   const governanceContracts = await deployGovernanace(owner, wallets);
   await initializeGovernance(governanceContracts);
 }
 
-export default setup;
+export default setupFixture;
