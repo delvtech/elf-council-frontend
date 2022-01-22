3bccbbefa8f7eeb454a414354514a93f1bce8372
Danny Delott <dannydelott@gmail.com>
2022-01-21 20:28:56 -0800

    Make go to dashboard button more prevalent

--- src/ui/airdrop/ClaimSuccessful/ClaimSuccessful.tsx
+++ src/ui/airdrop/ClaimSuccessful/ClaimSuccessful.tsx
@@ -10,27 +10,46 @@ import { ElementIcon, IconSize } from "src/ui/base/ElementIcon";
 import Link from "next/link";
 
 const elfiLogoElement = (
-  <ElementIcon key="element-icon" className="mx-2" size={IconSize.MEDIUM} />
+  <ElementIcon
+    key="element-icon"
+    className="mx-1 md:mx-2"
+    size={IconSize.MEDIUM}
+  />
 );
 export function ClaimSuccessful(): ReactElement {
   return (
     <Card
       variant={CardVariant.BLUE}
-      className="flex flex-col text-white text-center h-full"
+      className="flex flex-col h-full text-center text-white"
     >
-      <div className="flex flex-col p-6 justify-center items-center">
-        <h2 className="text-3xl font-bold my-6">{t`Claim Successful!`}</h2>
-        <div className="flex flex-col w-full items-center text-base space-y-3 mb-10">
-          <div className={"w-full md:w-1/2"}>
-            <p className="flex justify-center">{jt`Congratulations on claiming your ${elfiLogoElement} $ELFI tokens.`}</p>
-          </div>
-          <p className="w-full md:w-1/2">{t`Share your airdrop experience on Twitter and join the Element discord 
-	  to get more involved in our community and governance system.`}</p>
+      <div className="flex flex-col items-center justify-center p-6">
+        <h2 className="mb-6 text-3xl font-bold">{t`Claim Successful!`}</h2>
+        <div className="flex flex-col items-center w-full mb-10 space-y-3 text-base">
+          <p className="flex justify-center font-bold">{jt`Congratulations on claiming your ${elfiLogoElement} $ELFI tokens.`}</p>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-5/6 mb-10">
+        <Link href="/">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a className="w-2/3 mb-10">
+            <CallToActionCard
+              label={t`Visit Element Council Governance`}
+              icon={
+                <Image
+                  height="24px"
+                  width="24px"
+                  src="/assets/CouncilLogo.svg"
+                  alt={t`Element Council logo`}
+                />
+              }
+            />
+          </a>
+        </Link>
+        <p className="w-full mb-4 text-justify md:w-2/3">{t`Share your airdrop experience on Twitter and join the Element discord 
+	  to get more involved in our community and governance system.`}</p>
+        <div className="flex w-full space-x-4">
           <a
             target="_blank"
             rel="noreferrer"
+            className="flex-1"
             href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
               t`🧝‍♂️`,
             )}`}
@@ -40,37 +59,23 @@ export function ClaimSuccessful(): ReactElement {
               icon={<TwitterIconFromFigma />}
             />
           </a>
-          <a target="_blank" rel="noreferrer" href="https://element.fi/discord">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://element.fi/discord"
+            className="flex-1"
+          >
             <CallToActionCard
               label={t`Join #ELFArmy Discord`}
               icon={<DiscordIconFromFigma />}
             />
           </a>
-          <a href="https://forum.element.fi">
+          <a href="https://forum.element.fi" className="flex-1">
             <CallToActionCard
               label={t`Join the discussion`}
               icon={<CommonwealthIcon />}
             />
           </a>
-          <Link href="/">
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a>
-              <CallToActionCard
-                label={t`Launch dashboard`}
-                icon={
-                  <Image
-                    height="24px"
-                    width="24px"
-                    src="/assets/CouncilLogo.svg"
-                    alt={t`Element Council logo`}
-                  />
-                }
-              />
-            </a>
-          </Link>
-        </div>
-        <div className="flex w-full justify-center text-base">
-          <p className="w-full md:w-1/2">{t`To change your delegation, you can visit our delegate dashboard live in our main governance system.`}</p>
         </div>
       </div>
     </Card>
