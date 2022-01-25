eba08f642b2c7a4c767ac6a520f5a30af0f1c7cf
Danny Delott <dannydelott@gmail.com>
2022-01-24 16:24:28 -0800

    Show 4 rows, move textbox to the bottom

--- src/ui/airdrop/ChooseDelegate/ChooseDelegate.tsx
+++ src/ui/airdrop/ChooseDelegate/ChooseDelegate.tsx
@@ -2,6 +2,9 @@ import React, { ReactElement, useCallback, useEffect, useState } from "react";
 import { isValidAddress } from "src/base/isValidAddress";
 import { delegates } from "src/elf-council-delegates/delegates";
 import { StepCard } from "src/ui/airdrop/StepCard/StepCard";
+import Button from "src/ui/base/Button/Button";
+import { ButtonVariant } from "src/ui/base/Button/styles";
+import H1 from "src/ui/base/H1";
 import TextInput from "src/ui/base/Input/TextInput";
 import DelegateProfile from "src/ui/delegate/DelegatesList/DelegateProfile";
 import { t } from "ttag";
@@ -47,50 +50,51 @@ export function ChooseDelegate({
       className="relative"
       onNextStep={onNextStep}
       nextStepDisabled={!isValidAddress(delegateAddress || "")}
-      nextStepLabel={t`Review Claim`}
+      nextStepLabel={t`Review deposit`}
       onPrevStep={onPrevStep}
     >
-      <div className="text-left text-2xl font-bold mb-10">{t`Choose a delegate`}</div>
-      <div className="flex flex-col w-full justify-center text-base mb-10">
-        <span
-          className={"w-full mb-4"}
-        >{t`Select a community member to represent you in our governance system.
-        You can change this delegation at any time. If you want to learn more
-        about potential delegates, click on the x icon to learn more about them
-        or click on the y icon to view their Twitter.`}</span>
-        <span className="w-full font-bold mb-2">{t`You can delegate to someone
-        not listed or to yourself, by entering an Ethereum address.`}</span>
-      </div>
-      <div className="space-y-4">
-        <TextInput
-          screenReaderLabel={t`Enter delegate address`}
-          id={"delegate-address"}
-          name={t`Enter delegate address`}
-          placeholder={t`Enter delegate address`}
-          className="mb-4 h-12 text-left text-principalRoyalBlue placeholder-principalRoyalBlue"
-          value={delegateAddress}
-          onChange={(event) => setDelegateAddress(event.target.value)}
-        />
-        <div className="pr-1 rounded-xl shadow h-48 overflow-auto">
-          {/* List of delegates */}
-          <ul className="grid grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-3">
-            {delegates.map((delegate, idx) => {
-              const handleSelectDelegate = () => {
-                setSelectedDelegateIndex(idx);
-                setDelegateAddress(delegate.address);
-              };
+      <div className="flex flex-col items-center justify-center w-full h-full space-y-8">
+        <H1>{t`Choose a delegate`}</H1>
+        <div className="space-y-4">
+          <div className="pr-1 overflow-auto shadow h-80 rounded-xl">
+            {/* List of delegates */}
+            <ul className="grid grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-3">
+              {delegates.map((delegate, idx) => {
+                const handleSelectDelegate = () => {
+                  setSelectedDelegateIndex(idx);
+                  setDelegateAddress(delegate.address);
+                };
 
-              return (
-                <li key={`${delegate.address}-${idx}}`}>
-                  <DelegateProfile
-                    selected={idx === selectedDelegateIndex}
-                    delegate={delegate}
-                    onSelectDelegate={handleSelectDelegate}
-                  />
-                </li>
-              );
-            })}
-          </ul>
+                return (
+                  <li key={`${delegate.address}-${idx}}`}>
+                    <DelegateProfile
+                      selected={idx === selectedDelegateIndex}
+                      delegate={delegate}
+                      onSelectDelegate={handleSelectDelegate}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          <div className="flex flex-col">
+            <span>{t`Or type in an address of your choice`}</span>
+            <div className="flex space-x-4">
+              <Button
+                variant={ButtonVariant.OUTLINE_WHITE}
+              >{t`Self-delegate`}</Button>
+              <TextInput
+                screenReaderLabel={t`Enter delegate address`}
+                id={"delegate-address"}
+                name={t`Enter delegate address`}
+                placeholder={t`Enter delegate address`}
+                containerClassName="flex-1"
+                className="flex-1 h-12 mb-4 text-left text-principalRoyalBlue placeholder-principalRoyalBlue"
+                value={delegateAddress}
+                onChange={(event) => setDelegateAddress(event.target.value)}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </StepCard>
