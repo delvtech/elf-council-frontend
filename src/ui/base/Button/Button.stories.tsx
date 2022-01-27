c41f7d02ffb3daa1aa10abfeca8a3127e8e47ceb
Duke <hnguyen1179@berkeley.edu>
2022-01-27 14:37:32 -0800

    Convert tw

--- src/ui/base/Button/Button.stories.tsx
+++ src/ui/base/Button/Button.stories.tsx
@@ -1,21 +1,23 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Button from "./Button";
+import ButtonComponent from "./Button";
 import { ButtonVariant } from "src/ui/base/Button/styles";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: "Button",
-  component: Button,
-} as ComponentMeta<typeof Button>;
+  component: ButtonComponent,
+} as ComponentMeta<typeof ButtonComponent>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof ButtonComponent> = (args) => (
+  <ButtonComponent {...args} />
+);
 
-export const Primary = Template.bind({});
+export const Button = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
+Button.args = {
   children: <span>Click me</span>,
   variant: ButtonVariant.PRIMARY,
 };
