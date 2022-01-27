77e39d4f10172a3bf4d6bd837afbc36797560708
Duke <hnguyen1179@berkeley.edu>
2022-01-27 14:55:36 -0800

    Fix file structure

--- src/ui/base/Button/Button.stories.tsx
+++ src/ui/base/Button/Button.stories.tsx
@@ -6,7 +6,7 @@ import { ButtonVariant } from "src/ui/base/Button/styles";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Button",
+  title: "Button/Button",
   component: ButtonComponent,
 } as ComponentMeta<typeof ButtonComponent>;
 
