69c499ee586caab2bad27f415e6211831ca54b56
Danny Delott <dannydelott@gmail.com>
2022-01-28 12:51:01 -0800

    Add comment

--- src/ui/base/Intent.tsx
+++ src/ui/base/Intent.tsx
@@ -1,3 +1,11 @@
+/**
+ * The basic intents.
+ *
+ * These intents are used to change the look and feel of base components.
+ *
+ * Inspiration from blueprint UI's Intent system:
+ * https://github.com/palantir/blueprint/blob/develop/packages/core/src/common/intent.ts
+ */
 export enum Intent {
   WARNING = "warning",
   PRIMARY = "primary",
