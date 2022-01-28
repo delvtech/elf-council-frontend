a0c8c45c017f94020c58f5b60d432d6c00879b8b
Matthew Brown <mattfdbrown@gmail.com>
2022-01-28 07:53:45 -0800

    Update Overview Stats (#389)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import OverviewPage from "src/ui/overview/OverviewPage";
 import React, { ReactElement } from "react";
 import PageView from "src/ui/app/PageView";
+import { ProposalsJson } from "elf-council-proposals";
+import { PROPOSALS_JSON_URL } from "src/elf-council-proposals";
 
-export default function Home(): ReactElement {
+interface HomeProps {
+  proposalsJson: ProposalsJson;
+}
+export default function Home({ proposalsJson }: HomeProps): ReactElement {
   return (
     <PageView childrenContainerClassName="flex justify-center">
-      <OverviewPage />
+      <OverviewPage proposalsJson={proposalsJson} />
     </PageView>
   );
+}
+
+export async function getServerSideProps(): Promise<{
+  props: { proposalsJson: ProposalsJson };
+}> {
+  const res = await fetch(PROPOSALS_JSON_URL);
+  const proposalsJson = await res.json();
+  return { props: { proposalsJson } };
 }
