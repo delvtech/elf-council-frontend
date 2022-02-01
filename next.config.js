e87aebf877e43792b040dd491a49317507fe8115
Ryan Goree <goree.ryan@gmail.com>
2022-02-01 13:06:20 -0600

    Use zk lib for public ID generation (#404)
    * add 0x prefix to generateHashSeed
    
    * Install zk lib
    
    * ignore server-side packages imported in zk lib
    
    * Use lib to generate public id
    
    * Add missing dependency for zk lib

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@ const nextConfig = {
   eslint: {
     dirs: ["pages", "src"],
   },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      fs: false,
+      stream: false,
+      path: false,
+      crypto: false,
+      os: false,
+      http: false,
+      https: false,
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig;
