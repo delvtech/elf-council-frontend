6e13299b43fb64645d73acdd730d082365d2091e
Danny Delott <dannydelott@gmail.com>
2022-01-31 17:18:50 -0800

    [airdrop] Re-word "deposit" -> "delegate" based on feedback (#401)

--- src/ui/airdrop/AirdropPreview/AirdropPreview.tsx
+++ src/ui/airdrop/AirdropPreview/AirdropPreview.tsx
@@ -54,7 +54,7 @@ export function AirdropPreview({
           <span className="text-center">{t`Congratulations!`}</span>
         </div>
         <div className="flex flex-col items-center justify-center w-full mb-10 text-base text-center">
-          <span className="w-full mb-6 font-bold ">{jt`You have some ${elementIconInBodyText}ELFI available to deposit.`}</span>
+          <span className="w-full mb-6 font-bold ">{jt`You have some ${elementIconInBodyText}ELFI available to delegate.`}</span>
           <span className="w-3/4 text-justify">{t`You have received these tokens for being an active member of the Element community.
 We hope to see you continue to contribute to the future of Element.`}</span>
         </div>
