a7f9ad3b2af44ce96a6091afc49f641916925c7e
Danny Delott <dannydelott@gmail.com>
2022-02-04 17:06:56 -0800

    convert H2

--- src/ui/base/H2/H2.tsx
+++ src/ui/base/H2/H2.tsx
@@ -1,6 +1,5 @@
 import classNames from "classnames";
 import { ReactElement, ReactNode } from "react";
-import tw, { fontSize, fontWeight } from "src/elf-tailwindcss-classnames";
 
 interface H2Props {
   className?: string;
@@ -8,13 +7,6 @@ interface H2Props {
 }
 export default function H2({ className, children }: H2Props): ReactElement {
   return (
-    <h2
-      className={classNames(
-        tw(fontSize("text-xl"), fontWeight("font-bold")),
-        className,
-      )}
-    >
-      {children}
-    </h2>
+    <h2 className={classNames("text-xl font-bold", className)}>{children}</h2>
   );
 }
