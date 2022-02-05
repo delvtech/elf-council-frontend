ff74eff4b1ea9aec9bb19949474f15e16497c3ec
Danny Delott <dannydelott@gmail.com>
2022-02-04 16:33:49 -0800

    [airdrop] Update to latest wording of DelegateInstructions card (#426)

--- src/ui/airdrop/DelegateInstructions/DelegateInstructions.tsx
+++ src/ui/airdrop/DelegateInstructions/DelegateInstructions.tsx
@@ -1,7 +1,8 @@
 import React, { ReactElement } from "react";
 import { StepCard } from "src/ui/airdrop/StepCard/StepCard";
+import { ElementIcon, IconSize } from "src/ui/base/ElementIcon/ElementIcon";
 import H1 from "src/ui/base/H1/H1";
-import { t } from "ttag";
+import { jt, t } from "ttag";
 
 interface DelegateInstructionsProps {
   account: string | null | undefined;
@@ -9,6 +10,13 @@ interface DelegateInstructionsProps {
   onPrevStep: () => void;
 }
 
+const elementIconInBodyText = (
+  <ElementIcon
+    key="element-icon-in-body-text"
+    className="bg-paleLily inline-block ml-0.5 mr-1 -mb-1.5"
+    size={IconSize.MEDIUM}
+  />
+);
 export function DelegateInstructions({
   onNextStep,
   onPrevStep,
@@ -24,9 +32,12 @@ export function DelegateInstructions({
         <H1>{t`Vote or delegate?`}</H1>
         <p
           className={"w-2/3 mb-8"}
-        >{t`As an $ELFI depositor you can use your voting power to participate
-        in governance, or you can delegate it to another member to represent you
-        and your vision. You can change your selection at any time.`}</p>
+        >{jt`As a participant in Element DAO, you can use your
+        ${elementIconInBodyText}ELFI voting power yourself, or you can delegate
+        it to another member to represent you and your vision. You can only
+        delegate it to one user, but you can change your selection at any
+        time.`}</p>
+
         <p className="w-2/3 mb-2 font-bold">{t`In all cases you maintain
         ownership of your tokens, which will remain in the Voting Vault.`}</p>
       </div>
