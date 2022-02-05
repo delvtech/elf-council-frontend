61e71058ab038297313280452f0a249570e85738
Danny Delott <dannydelott@gmail.com>
2022-02-04 17:08:32 -0800

    convert pages/delegate.tsx

--- pages/delegate.tsx
+++ pages/delegate.tsx
@@ -1,11 +1,10 @@
 import React, { ReactElement } from "react";
-import { width } from "src/elf-tailwindcss-classnames";
 import PageView from "src/ui/app/PageView";
 import DelegatePage from "src/ui/delegate/DelegatePage";
 
 export default function Delegates(): ReactElement {
   return (
-    <PageView childrenContainerClassName={width("w-full")}>
+    <PageView childrenContainerClassName={"w-full"}>
       <DelegatePage />
     </PageView>
   );
