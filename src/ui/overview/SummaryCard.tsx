fae9b9ade1d0cd9e4eab563188f4a59783bdc9c6
Danny Delott <dannydelott@gmail.com>
2022-02-07 17:30:27 -0800

    Make Active Proposals link to proposals page (#437)

--- src/ui/overview/SummaryCard.tsx
+++ src/ui/overview/SummaryCard.tsx
@@ -1,32 +1,27 @@
+import { InformationCircleIcon } from "@heroicons/react/outline";
 import React, { ReactElement, ReactNode } from "react";
-
-import { t } from "ttag";
 
 import Card from "src/ui/base/Card/Card";
 import Tooltip from "src/ui/base/Tooltip/Tooltip";
-import { RESOURCES_URL } from "src/ui/resources";
 
 interface SummaryCardProps {
-  title: string;
+  title: string | ReactElement;
+  tooltipContent?: string | ReactElement;
   balance?: ReactNode;
   children?: ReactNode;
 }
 export function SummaryCard(props: SummaryCardProps): ReactElement {
-  const { title, balance, children } = props;
+  const { title, balance, children, tooltipContent } = props;
 
   return (
     <Card className="flex flex-col lg:flex-1">
-      <div className="-mt-2 text-sm font-light text-principalRoyalBlue">
-        <Tooltip content={t`Click to find out more.`}>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href={RESOURCES_URL}
-            className="underline"
-          >
-            {title}
-          </a>
-        </Tooltip>
+      <div className="flex items-center justify-center text-principalRoyalBlue">
+        {title}
+        {tooltipContent ? (
+          <Tooltip content={tooltipContent}>
+            <InformationCircleIcon className="h-4 ml-1 cursor-help" />
+          </Tooltip>
+        ) : null}
       </div>
       <div className="flex items-center justify-center flex-1 py-4 text-5xl text-center font-extralight text-principalRoyalBlue">
         {balance}
