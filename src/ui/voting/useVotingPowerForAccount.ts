f9550ae16e7d8bd7c07c73764e527e23383ea4f1
Matthew Brown <mattfdbrown@gmail.com>
2022-02-08 08:48:12 -0800

    Fix useLockingVaultVotePower CU-2a16qu5 (#442)
    * fix locking vault vote power
    
    * remove nonFungibleVotePower
    
    * update react-query-typechain
    
    * use useSmartContractReadCall

--- src/ui/voting/useVotingPowerForAccount.ts
+++ src/ui/voting/useVotingPowerForAccount.ts
@@ -7,29 +7,11 @@ export function useVotingPowerForAccount(
   account: string | undefined | null,
   atBlockNumber?: number,
 ): string {
-  // NOTE: the following are commented out but we'll need to add them in soon.
-  // The CoreVoting contract passes this parameter to the rewards contract when querying vote power,
-  // so we have to pass it as well, though the rewards contract doesn't need this to calculate vote
-  // power.
-  // const rewardsVotingPower = useRewardsVaultVotingPower(
-  //   account,
-  //   rewardsContract,
-  //   atBlockNumber,
-  // );
-
-  // const nonFungibleVotingPower = useRewardsVaultVotingPower(
-  //   account,
-  //   nonFungibleVotingContract,
-  //   atBlockNumber,
-  // );
-
   const vestingVaultPower = useVestingVaultVotingPower(account, atBlockNumber);
   const lockingVotingPower = useLockingVaultVotingPower(account, atBlockNumber);
 
   const votingPower = FixedNumber.from(lockingVotingPower)
     .addUnsafe(FixedNumber.from(vestingVaultPower || "0"))
-    // .addUnsafe(FixedNumber.from(rewardsVotingPower))
-    // .addUnsafe(FixedNumber.from(nonFungibleVotingPower))
     .toString();
 
   return votingPower;
