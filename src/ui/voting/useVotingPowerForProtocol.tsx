00c898c96c13f8eaab537b4fd43dfcb449d4673b
Matthew Brown <mattfdbrown@gmail.com>
2022-02-08 14:27:11 -0800

    update useVotingPowerForProtocol
    now uses events instead of balance.

--- src/ui/voting/useVotingPowerForProtocol.tsx
+++ src/ui/voting/useVotingPowerForProtocol.tsx
@@ -1,10 +1,14 @@
-import { useSmartContractReadCall } from "@elementfi/react-query-typechain";
-import { BigNumber } from "ethers";
+import {
+  useSmartContractEvents,
+  useSmartContractReadCall,
+} from "@elementfi/react-query-typechain";
+import { BigNumber, ethers } from "ethers";
 import { formatEther } from "ethers/lib/utils";
+import { useMemo } from "react";
 import { addressesJson } from "src/elf-council-addresses";
 import { elementTokenContract, vestingContract } from "src/elf/contracts";
 
-const { vestingVault, lockingVault } = addressesJson.addresses;
+const { lockingVault } = addressesJson.addresses;
 
 export function useVotingPowerForProtocol(): number {
   // value in Eth
@@ -17,27 +21,47 @@ export function useVotingPowerForProtocol(): number {
 
   const lockingVaultVotingPower = +formatEther(lockingVaultBalanceBN);
 
-  const votingPower = vestingVaultVotingPower + lockingVaultVotingPower;
+  const votingPower = +vestingVaultVotingPower + lockingVaultVotingPower;
 
   return votingPower;
 }
 
-// TODO: initially the vesting vault has only unvested tokens, so we can get away with this simple
-// calculation.  however, around December of 2022 this will no long be the case and we'll have to
-// update this calculation to include unvested/invested portions per grant in the vault
-function useVestingVaultVotingPower() {
-  const { data: vestingVaultBalanceBN = BigNumber.from(0) } =
-    useSmartContractReadCall(elementTokenContract, "balanceOf", {
-      callArgs: [vestingVault],
+function useVestingVaultVotingPower(): string {
+  const { data: events } = useSmartContractEvents(
+    vestingContract,
+    "VoteChange",
+  );
+
+  return useMemo(() => {
+    if (!events) {
+      return "0";
+    }
+
+    // tally vote power for each address
+    const votingPowerByAddress: Record<string, BigNumber> = {};
+    events.forEach((event) => {
+      const [delegatee, , amount]: [string, string, BigNumber] =
+        (event.args as [string, string, BigNumber]) || [
+          ethers.constants.AddressZero,
+          ethers.constants.AddressZero,
+          BigNumber.from(0),
+        ];
+
+      const previousVotingPower =
+        votingPowerByAddress[delegatee] || BigNumber.from(0);
+      votingPowerByAddress[delegatee] = previousVotingPower.add(amount);
     });
 
-  const { data: vestingVaultMultiplierBN = BigNumber.from(0) } =
-    useSmartContractReadCall(vestingContract, "unvestedMultiplier", {
-      callArgs: [],
+    // sum up vote power across all addresses
+    let vestingVaultVotingPowerBN = BigNumber.from(0);
+    Object.keys(votingPowerByAddress).forEach((address) => {
+      const votePowerBN = votingPowerByAddress[address];
+      vestingVaultVotingPowerBN = vestingVaultVotingPowerBN.add(votePowerBN);
     });
-  const unvestedMultiplier = vestingVaultMultiplierBN?.toNumber();
-  const vestingVaultBalance = +formatEther(vestingVaultBalanceBN);
-  const vestingVaultVotingPower =
-    (vestingVaultBalance * unvestedMultiplier) / 100;
-  return vestingVaultVotingPower;
+
+    const vestingVaultVotingPower = formatEther(vestingVaultVotingPowerBN);
+    return vestingVaultVotingPower;
+    // events are immutable, only update if there are new events.
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [events?.length]);
 }
