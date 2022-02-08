a6be73970069aa87e2cf1a647492671d14aef4dc
Danny Delott <dannydelott@gmail.com>
2022-02-08 13:13:19 -0800

    Split this function up into to single-purpose utils

--- src/ui/voting/useVotingPowerForAccount.ts
+++ src/ui/voting/useVotingPowerForAccount.ts
@@ -1,9 +1,28 @@
 import { FixedNumber } from "ethers";
+import { useLatestBlockNumber } from "src/ui/ethereum/useLatestBlockNumber";
 
 import { useLockingVaultVotingPower } from "src/ui/voting/useLockingVaultVotingPower";
 import { useVestingVaultVotingPower } from "src/ui/voting/useVestingVaultVotingPower";
 
-export function useVotingPowerForAccount(
+export function useVotingPowerForAccountAtLatestBlock(
+  account: string | undefined | null,
+): string {
+  const { data: latestBlockNumber } = useLatestBlockNumber();
+  return useVotingPowerForAccount(account, latestBlockNumber);
+}
+
+/**
+ * Useful for looking up vote power available for a proposal, where
+ * atBlockNumber is when the proposal was created.
+ */
+export function useVotingPowerForAccountAtBlockNumber(
+  account: string | undefined | null,
+  atBlockNumber: number,
+): string {
+  return useVotingPowerForAccount(account, atBlockNumber);
+}
+
+function useVotingPowerForAccount(
   account: string | undefined | null,
   atBlockNumber?: number,
 ): string {
