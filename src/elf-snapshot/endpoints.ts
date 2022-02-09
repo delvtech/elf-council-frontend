ed25b9f9df11a2caec6fdc64c6aa742fc1fdc5ed
Danny Delott <dannydelott@gmail.com>
2022-02-09 15:15:13 -0800

    Formalize snapshot url for goerli/mainnet

--- src/elf-snapshot/endpoints.ts
+++ src/elf-snapshot/endpoints.ts
@@ -1,3 +1,16 @@
+import { addressesJson } from "src/elf-council-addresses";
+import { ChainId } from "src/ethereum";
+
 export const SNAPSHOT_GRAPHQL_ENDPOINT = "https://hub.snapshot.org/graphql";
-export const ELEMENT_FINANCE_SNAPSHOT_URL =
-  "https://snapshot.org/#/element-finance.eth";
+export const ELEMENT_FINANCE_SNAPSHOT_URL = getSnapshotUrl();
+
+function getSnapshotUrl() {
+  switch (addressesJson.chainId) {
+    case ChainId.MAINNET:
+      return "https://snapshot.org/#/element-finance.eth";
+    case ChainId.GOERLI:
+    case ChainId.LOCAL:
+    default:
+      return "https://snapshot.org/#/element-finance-goerli.eth";
+  }
+}
