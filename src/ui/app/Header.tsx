83c2465823bb29002e44c5f8ea4b1a11e7e4df6d
Ryan Goree <goree.ryan@gmail.com>
2022-02-11 13:50:03 -0600

    Use span for inline elements

--- src/ui/app/Header.tsx
+++ src/ui/app/Header.tsx
@@ -20,7 +20,7 @@ function Header(): ReactElement {
       <div className="mr-3 flex items-center space-x-4 text-gray-400">
         {account ? (
           <div className="flex items-center">
-            <div className="mr-8 flex items-center gap-1">
+            <span className="mr-8 flex items-center gap-1">
               <a
                 target="_blank"
                 rel="noreferrer"
@@ -30,20 +30,20 @@ function Header(): ReactElement {
                 <span className="text-principalRoyalBlue">{t`Learn how to vote`}</span>
                 <ExternalLinkIcon className="h-4 shrink-0 text-principalRoyalBlue" />
               </a>
-            </div>
+            </span>
             <a
               href={GAS_URL}
               target="_blank"
               rel="noreferrer"
               className="mr-8 flex items-center"
             >
-              <div className="relative h-5 w-5">
+              <span className="relative h-5 w-5">
                 <Image
                   layout="fill"
                   src="/assets/gas.svg"
                   alt={t`Gas pump icon`}
                 />
-              </div>
+              </span>
               <span className="ml-2 mr-1 font-bold text-principalRoyalBlue">
                 {gasPrice?.recommendedBaseFee || 0.0}
               </span>
