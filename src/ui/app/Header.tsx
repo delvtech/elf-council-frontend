c590929d3f2a3aad3dfef0885b8997c60a4c1be0
Ryan Goree <goree.ryan@gmail.com>
2022-02-11 14:24:18 -0600

    Add global ELFI widget (#487)
    * Use span for inline elements
    
    * Add global ELFI widget

--- src/ui/app/Header.tsx
+++ src/ui/app/Header.tsx
@@ -6,13 +6,18 @@ import { t } from "ttag";
 import { WalletProfileButton } from "src/ui/wallet/ConnectWalletButton";
 import { useGasPrice } from "src/ui/ethereum/useGasPrice";
 import { RESOURCES_URL } from "src/ui/resources";
+import { useDeposited } from "src/ui/base/lockingVault/useDeposited";
+import { ElementIcon, IconSize } from "src/ui/base/ElementIcon/ElementIcon";
+import Tooltip from "src/ui/base/Tooltip/Tooltip";
 import { Provider } from "@ethersproject/providers";
 
 const GAS_URL = "https://www.etherchain.org/tools/gasnow";
+const elfiTooltipText = t`The amount of voting power you own in the system`;
 
 function Header(): ReactElement {
   const { account, active, library } = useWeb3React<Provider>();
   const { data: gasPrice } = useGasPrice();
+  const amountDeposited = useDeposited(account) || "0";
 
   return (
     <div className="flex w-full justify-between">
@@ -20,7 +25,7 @@ function Header(): ReactElement {
       <div className="mr-3 flex items-center space-x-4 text-gray-400">
         {account ? (
           <div className="flex items-center">
-            <div className="mr-8 flex items-center gap-1">
+            <span className="mr-8 flex items-center gap-1">
               <a
                 target="_blank"
                 rel="noreferrer"
@@ -30,24 +35,33 @@ function Header(): ReactElement {
                 <span className="text-principalRoyalBlue">{t`Learn how to vote`}</span>
                 <ExternalLinkIcon className="h-4 shrink-0 text-principalRoyalBlue" />
               </a>
-            </div>
+            </span>
             <a
               href={GAS_URL}
               target="_blank"
               rel="noreferrer"
               className="mr-8 flex items-center"
             >
-              <div className="relative h-5 w-5">
+              <span className="relative h-5 w-5">
                 <Image
                   layout="fill"
                   src="/assets/gas.svg"
                   alt={t`Gas pump icon`}
                 />
-              </div>
+              </span>
               <span className="ml-2 mr-1 font-bold text-principalRoyalBlue">
                 {gasPrice?.recommendedBaseFee || 0.0}
               </span>
             </a>
+            <Tooltip content={elfiTooltipText}>
+              <span className="mr-8 flex items-center gap-2 font-bold text-principalRoyalBlue">
+                <ElementIcon size={IconSize.MEDIUM} />
+                <span>
+                  {amountDeposited}
+                  <span className="hidden md:inline"> ELFI</span>
+                </span>
+              </span>
+            </Tooltip>
           </div>
         ) : null}
 
