5b786b756ac3f83decbd7ee66c316d321776b2e5
Duke <hnguyen1179@berkeley.edu>
2022-02-15 14:56:07 -0800

    Update sidebar to match new design

--- src/ui/app/Sidebar.tsx
+++ src/ui/app/Sidebar.tsx
@@ -1,6 +1,12 @@
 import React, { Fragment, ReactElement, useCallback, useState } from "react";
-import { MenuAlt4Icon } from "@heroicons/react/solid";
-import { ExternalLinkIcon, XIcon } from "@heroicons/react/solid";
+import {
+  MenuAlt4Icon,
+  ExternalLinkIcon,
+  XIcon,
+  HomeIcon,
+  PencilAltIcon,
+  UserGroupIcon,
+} from "@heroicons/react/solid";
 import Link from "next/link";
 import { useRouter, NextRouter } from "next/router";
 import classNames from "classnames";
@@ -46,7 +52,7 @@ export default function Sidebar(props: SidebarProps): ReactElement {
           "fixed top-0 left-0 z-10 flex h-full w-full transform-gpu flex-col items-center justify-between bg-white py-14 transition-all duration-300 ease-in-out md:w-60 md:translate-x-0",
         )}
       >
-        <div>
+        <div className="w-full">
           <div className="mt-1 flex justify-around py-3">
             <div className="relative h-24 w-24">
               <Image
@@ -62,14 +68,31 @@ export default function Sidebar(props: SidebarProps): ReactElement {
               <XIcon className="h-6 w-6" />
             </button>
           </div>
-          <div className="mt-16 w-full space-y-6">
-            <SidebarLink link="/" label={t`Overview`} router={router} />
+          <div className="mt-16 space-y-6">
+            <SidebarLink
+              link="/"
+              label={t`Overview`}
+              router={router}
+              icon={
+                <HomeIcon className="h-4 w-4 flex-shrink-0 text-principalRoyalBlue" />
+              }
+            />
             <SidebarLink
               link="/proposals"
               label={t`Proposals`}
               router={router}
+              icon={
+                <PencilAltIcon className="h-4 w-4 flex-shrink-0 text-principalRoyalBlue" />
+              }
             />
-            <SidebarLink link="/delegate" label={t`Delegate`} router={router} />
+            <SidebarLink
+              link="/delegate"
+              label={t`Delegate`}
+              router={router}
+              icon={
+                <UserGroupIcon className="h-4 w-4 flex-shrink-0 text-principalRoyalBlue" />
+              }
+            />
             <SidebarLinkExternal
               link="https://forum.element.fi"
               label={t`Forum`}
@@ -95,6 +118,7 @@ interface SidebarLinkProps {
   link: string;
   label: string;
   router: NextRouter;
+  icon?: ReactElement;
 }
 
 interface SidebarLinkExternalProps {
@@ -109,7 +133,7 @@ function AirdropLink(props: AirdropLinkProps): ReactElement {
   const { link } = props;
 
   return (
-    <div className="text-center">
+    <div className="text-left">
       {/* Don't use the next router for this, since the user might already be on
       the airdrop page when they click this. This way, users can always go back
       to the beginning of the airdrop by clicking the sidebar link */}
@@ -120,22 +144,23 @@ function AirdropLink(props: AirdropLinkProps): ReactElement {
   );
 }
 function SidebarLink(props: SidebarLinkProps): ReactElement {
-  const { link, label, router } = props;
+  const { link, label, router, icon } = props;
 
   const isActive = router.pathname === link;
 
   return (
-    <div>
+    <div className="flex justify-center hover:bg-blue-50">
       <Link href={link}>
         {/* There's a big discussion about how awful the Link api is for a11y
       here: https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/issues/402 the
       best thing to do for now is just ignore this rule when an anchor tag is
       the child of a Link since all a tags *should* have an href 🙁 */
         /* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a>
+        <a className="flex items-center md:w-1/2">
+          {icon}
           <div
             className={classNames(
-              "flex cursor-pointer justify-center p-3 text-brandDarkBlue-dark hover:bg-blue-50",
+              "flex cursor-pointer justify-start p-3 text-brandDarkBlue-dark",
               { "font-bold": isActive },
             )}
           >
@@ -150,18 +175,18 @@ function SidebarLink(props: SidebarLinkProps): ReactElement {
 function SidebarLinkExternal(props: SidebarLinkExternalProps): ReactElement {
   const { link, label } = props;
   return (
-    <a
-      href={link}
-      target="_blank"
-      rel="noreferrer"
-      className="flex w-full items-center justify-center hover:bg-blue-50"
-    >
-      {/* Empty span w/ same width as icon to center the label text */}
-      <span className="w-4" />
-      <div className="flex cursor-pointer justify-center p-3 text-brandDarkBlue-dark">
-        <p>{label}</p>
-      </div>
-      <ExternalLinkIcon className="h-4 w-4 text-principalRoyalBlue" />
-    </a>
+    <div className="flex justify-center hover:bg-blue-50">
+      <a
+        href={link}
+        target="_blank"
+        rel="noreferrer"
+        className="flex items-center md:w-1/2"
+      >
+        <ExternalLinkIcon className="h-4 w-4 text-principalRoyalBlue" />
+        <div className="flex cursor-pointer justify-center p-3 text-brandDarkBlue-dark">
+          <p>{label}</p>
+        </div>
+      </a>
+    </div>
   );
 }
