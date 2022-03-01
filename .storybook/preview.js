8b0d61d03ed519f3800a3799cb98a9008b1c4ce9
Duke <hnguyen1179@berkeley.edu>
2022-03-01 12:31:46 -0800

    Add documentation

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,9 @@ import { addDecorator } from "@storybook/react";
 import { withA11y } from "@storybook/addon-a11y";
 import "../styles/globals.css";
 
+// Chromatic storybook testing doesn't work well with the default optimized images from next/image.
+// Rather than accounting for this in each component, we'll just override next/image import
+// and force all consumers to be default unoptimized when storybook runs.
 const OriginalNextImage = NextImage.default;
 
 Object.defineProperty(NextImage, "default", {
