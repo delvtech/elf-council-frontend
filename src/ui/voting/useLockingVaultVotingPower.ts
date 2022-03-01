61aa031ecbc03b839fab50e5e619fa83133b0530
Danny Delott <dannydelott@gmail.com>
2022-02-28 16:43:20 -0800

    Refactor custom hooks

--- src/ui/voting/useLockingVaultVotingPower.ts
+++ src/ui/voting/useLockingVaultVotingPower.ts
@@ -1,28 +1,30 @@
-import { useSmartContractReadCall } from "@elementfi/react-query-typechain";
-import { formatEther } from "@ethersproject/units";
-
 import { lockingVaultContract } from "src/elf/contracts";
-import { useLatestBlockNumber } from "src/ui/ethereum/useLatestBlockNumber";
+import {
+  useQueryVotePower,
+  useQueryVotePowerView,
+} from "src/ui/voting/useQueryVotePower";
 
+/**
+ * Use this to get the current vote power.
+ *
+ * Voting power can go stale if the current block is beyond the staleBlockLag +
+ * atBlockNumber. In the case of stale voting power, this will return "0".
+ */
 export function useLockingVaultVotingPower(
   account: string | undefined | null,
   atBlockNumber?: number,
 ): string {
-  const { data: latestBlockNumber } = useLatestBlockNumber();
+  return useQueryVotePower(account, lockingVaultContract, atBlockNumber);
+}
 
-  const blockNumber = atBlockNumber || latestBlockNumber;
-
-  const { data: lockingVotingPowerBN } = useSmartContractReadCall(
-    lockingVaultContract,
-    "queryVotePower",
-    {
-      callArgs: [account as string, blockNumber as number, "0x00"],
-      enabled: !!account && !!blockNumber,
-      keepPreviousData: true,
-    },
-  );
-
-  const lockingVotingPower = formatEther(lockingVotingPowerBN || 0);
-
-  return lockingVotingPower;
+/**
+ * Use this to get the historical voting power.
+ *
+ * This does not take into account whether or not the voting power is stale.
+ */
+export function useLockingVaultVotingPowerView(
+  account: string | undefined | null,
+  atBlockNumber?: number,
+): string {
+  return useQueryVotePowerView(account, lockingVaultContract, atBlockNumber);
 }
