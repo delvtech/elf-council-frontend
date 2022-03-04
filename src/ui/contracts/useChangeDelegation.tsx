99451ed5f5ba98f67b2acac089c3a234555b115d
Duke <hnguyen1179@berkeley.edu>
2022-03-04 14:33:35 -0800

    Add etherscan link to tx loading toast

--- src/ui/contracts/useChangeDelegation.tsx
+++ src/ui/contracts/useChangeDelegation.tsx
@@ -9,6 +9,8 @@ import {
 } from "@elementfi/react-query-typechain";
 import { queryClient } from "src/elf/queryClient";
 import { useRef } from "react";
+import { ETHERSCAN_TRANSACTION_DOMAIN } from "src/elf-etherscan/domain";
+import { jt } from "ttag";
 
 export function useChangeDelegation(
   address: string | null | undefined,
@@ -25,13 +27,29 @@ export function useChangeDelegation(
     "changeDelegation",
     signer,
     {
+      // TODO: Add ttags to toast notifications
       onError: (e) => {
         toast.error(e.message, {
           id: toastIdRef.current,
         });
       },
-      onTransactionSubmitted: () => {
-        toastIdRef.current = toast.loading("Changing delegation");
+      onTransactionSubmitted: (tx) => {
+        const etherscanLink = (
+          <a
+            href={`${ETHERSCAN_TRANSACTION_DOMAIN}/${tx.hash}`}
+            target="_blank"
+            rel="noreferrer"
+            className="underline"
+          >
+            etherscan
+          </a>
+        );
+
+        const message = (
+          <div>{jt`Changing delegation... ${etherscanLink}`}</div>
+        );
+
+        toastIdRef.current = toast.loading(message);
       },
       onTransactionMined: () => {
         // Invalidate `deposits` so that consumers of `useDelegate` refresh
