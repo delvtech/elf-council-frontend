ae1c170e4fe7b773adad9c00d41488fd4c03e278
Ryan Goree <goree.ryan@gmail.com>
2022-03-04 15:05:17 -0600

    Improve useWorker - Refactor types - Resolve promises in the worker before posting - Add error handling - Add useWorkerPromise

--- src/ui/base/useWorker.ts
+++ src/ui/base/useWorker.ts
@@ -1,21 +1,35 @@
 import { useCallback, useMemo, useState } from "react";
 
-const workerFunction = <Args extends unknown[]>(
-  fn: (...args: Args) => void,
-  args: Args,
+type Callback = (...args: any[]) => any;
+
+const workerFunction = <TCallback extends Callback>(
+  fn: TCallback,
+  args: Parameters<TCallback>,
 ) => {
   // babel converts spread arrays to a function called _toConsumableArray which
   // isn't defined in the web worker.
   // eslint-disable-next-line prefer-spread
   const data = fn.apply(null, args);
-  // eslint-disable-next-line no-restricted-globals
-  self.postMessage(data);
+
+  // If async, return the resolved value instead of the promise.
+  // A better way to check if data is a promise would be
+  // `data instanceOf Promise`, but babel converts instanceOf to _instanceOf
+  // which isn't defined inside the worker.
+  if (data.then) {
+    data.then((res: ReturnType<TCallback>) => {
+      // eslint-disable-next-line no-restricted-globals
+      self.postMessage(res);
+    });
+  } else {
+    // eslint-disable-next-line no-restricted-globals
+    self.postMessage(data);
+  }
 };
 
-export function wrapInWorker<Args extends unknown[]>(
-  fn: (...args: Args) => void,
-): (...args: Args) => Worker {
-  return function (...args: Args) {
+export function wrapInWorker<TCallback extends Callback>(
+  fn: TCallback,
+): (...args: Parameters<TCallback>) => Worker {
+  return function (...args: Parameters<TCallback>) {
     // uses IIFE to automatically invoke the function in the worker.
     // uses JSON.stringify to ensure strings stay strings when concatenated in
     // the template literal.
@@ -29,18 +43,22 @@ export function wrapInWorker<Args extends unknown[]>(
   };
 }
 
-interface UseWorkerResult<TCallback extends (...args: any[]) => void> {
+interface UseWorkerResult<TCallback extends Callback> {
   run: (...args: Parameters<TCallback>) => void;
   loading: boolean;
-  data: ReturnType<TCallback> | undefined;
+  data?: Awaited<ReturnType<TCallback>>;
+  error?: ErrorEvent;
+  messageError?: MessageEvent;
 }
 
-export default function useWorker<TCallback extends (...args: any[]) => any>(
+export default function useWorker<TCallback extends Callback>(
   fn: TCallback,
 ): UseWorkerResult<TCallback> {
   const [state, setState] = useState<Omit<UseWorkerResult<TCallback>, "run">>({
     loading: false,
     data: undefined,
+    error: undefined,
+    messageError: undefined,
   });
 
   const wrappedFunction = useMemo(() => wrapInWorker(fn), [fn]);
@@ -52,6 +70,12 @@ export default function useWorker<TCallback extends (...args: any[]) => any>(
       worker.onmessage = ({ data }) => {
         setState({ loading: false, data });
       };
+      worker.onerror = (error) => {
+        setState({ loading: false, error });
+      };
+      worker.onmessageerror = (messageError) => {
+        setState({ loading: false, messageError });
+      };
     },
     [wrappedFunction],
   );
@@ -60,4 +84,21 @@ export default function useWorker<TCallback extends (...args: any[]) => any>(
     run,
     ...state,
   };
+}
+
+export function useWorkerPromise<TCallback extends Callback>(
+  fn: TCallback,
+): (...args: Parameters<TCallback>) => Promise<ReturnType<TCallback>> {
+  return useCallback(
+    (...args: Parameters<TCallback>) =>
+      new Promise((resolve, reject) => {
+        const worker = wrapInWorker(fn)(...args);
+        worker.onmessage = ({ data }) => {
+          resolve(data);
+        };
+        worker.onerror = reject;
+        worker.onmessageerror = reject;
+      }),
+    [fn],
+  );
 }
