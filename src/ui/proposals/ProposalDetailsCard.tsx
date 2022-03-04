266267311415b927ec66fdaaddfa26f046cd273a
Duke <hnguyen1179@berkeley.edu>
2022-03-04 14:46:33 -0800

    Add etherscan to vote tx

--- src/ui/proposals/ProposalDetailsCard.tsx
+++ src/ui/proposals/ProposalDetailsCard.tsx
@@ -19,7 +19,7 @@ import { Proposal } from "elf-council-proposals";
 import { Signer, ContractTransaction } from "ethers";
 import { commify, formatEther } from "ethers/lib/utils";
 import { isNumber } from "lodash";
-import { t } from "ttag";
+import { t, jt } from "ttag";
 
 import { assertNever } from "src/base/assertNever";
 import { getIsVotingOpen } from "src/elf-council-proposals";
@@ -124,7 +124,22 @@ export function ProposalDetailsCard(
       toast.error(t`${e.message}`, { id: toastIdRef.current });
     },
     onTransactionSubmitted: (pendingTransaction) => {
-      toastIdRef.current = toast.loading(t`Submitting vote`);
+      const pendingEtherscanLink = (
+        <a
+          href={`${ETHERSCAN_TRANSACTION_DOMAIN}/${pendingTransaction.hash}`}
+          target="_blank"
+          rel="noreferrer"
+          className="underline"
+        >
+          view etherscan
+        </a>
+      );
+
+      const message = (
+        <div>{jt`Submitting vote... ${pendingEtherscanLink}`}</div>
+      );
+
+      toastIdRef.current = toast.loading(message);
       setIsChangingVote(false);
       setIsVoteTxPending(true);
       setNewVoteTransaction(pendingTransaction);
