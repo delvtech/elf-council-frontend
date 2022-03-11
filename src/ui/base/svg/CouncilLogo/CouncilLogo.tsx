6e8708497895485383f82227fbbebab66fa4cebc
Ryan Goree <goree.ryan@gmail.com>
2022-03-11 15:51:34 -0600

    Fix broken import

--- src/ui/base/svg/CouncilLogo/CouncilLogo.tsx
+++ src/ui/base/svg/CouncilLogo/CouncilLogo.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { SVGComponentProps } from "src/ui/base/svg/types";
-import SVG from "./CouncilLogo.svg";
+import SVG from "./councilLogo.svg";
 
 export default function CouncilLogo(props: SVGComponentProps): ReactElement {
   return <SVG {...props} />;
