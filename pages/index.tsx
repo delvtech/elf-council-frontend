384ceaac770f6acff3b7eee0e8d37dabd748027b
Ryan Goree <goree.ryan@gmail.com>
2022-03-30 18:54:06 -0500

    Add lambda call from build server

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,9 +15,20 @@ export default function Home({ proposalsJson }: HomeProps): ReactElement {
   );
 }
 
-export async function getServerSideProps(): Promise<{
+export async function getStaticProps(): Promise<{
   props: { proposalsJson: ProposalsJson };
 }> {
+  const screenResult = await fetch(
+    "https://6zqnxzsgja.execute-api.us-east-2.amazonaws.com/screen",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        address: "0x00D5E029aFCE62738fa01EdCA21c9A4bAeabd434",
+      }),
+    },
+  ).then((res) => res.json());
+  console.log('SCREEN RESULT:', screenResult)
+
   const res = await fetch(PROPOSALS_JSON_URL);
   const proposalsJson = await res.json();
   return { props: { proposalsJson } };
