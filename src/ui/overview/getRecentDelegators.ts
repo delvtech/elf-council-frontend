e24b7842d174092373191d4beaf8ec15a6d9cd57
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-31 17:51:31 -0500

    Update getRecentDelegators.ts

--- src/ui/overview/getRecentDelegators.ts
+++ src/ui/overview/getRecentDelegators.ts
@@ -1,15 +1,9 @@
-import { defaultProvider as provider } from "src/elf/providers/providers";
 import {
   lockingVaultContract as lockingVault,
   vestingContract as vestingVault,
 } from "src/elf/contracts";
-import { Log } from "@ethersproject/providers";
-import { BigNumber } from "ethers";
 
 const STARTING_BLOCK_NUMBER = 14496292;
-
-const noTokens =
-  "0x0000000000000000000000000000000000000000000000000000000000000000";
 
 // Fetches the number of unique delegators from both locking and vesting vaults
 // This function should be used within NextJs getStaticProps with a TTL to cache this result
@@ -18,8 +12,14 @@ export async function getRecentDelegators(): Promise<string[]> {
   const lockingFilter = lockingVault.filters.VoteChange(null, null, null);
   const vestingFilter = vestingVault.filters.VoteChange(null, null, null);
 
-  const lockingEvents = await lockingVault.queryFilter(lockingFilter, 0);
-  const vestingEvents = await vestingVault.queryFilter(vestingFilter, 0);
+  const lockingEvents = await lockingVault.queryFilter(
+    lockingFilter,
+    STARTING_BLOCK_NUMBER,
+  );
+  const vestingEvents = await vestingVault.queryFilter(
+    vestingFilter,
+    STARTING_BLOCK_NUMBER,
+  );
 
   const delegators = new Set<string>([]);
 
