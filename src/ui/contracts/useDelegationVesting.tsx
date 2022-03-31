2a6dd0eede304e8ade8d25483689038324f2a836
Cash DeLeon <cashd@users.noreply.github.com>
2022-03-31 14:46:34 -0500

    useDelegationVesting hook

---
+++ src/ui/contracts/useDelegationVesting.tsx
@@ -0,0 +1,60 @@
+import { lockingVaultContract, vestingContract } from "src/elf/contracts";
+import { toast } from "react-hot-toast";
+import { ContractReceipt, Signer } from "ethers";
+import { UseMutationResult } from "react-query";
+import { LockingVault, VestingVault } from "elf-council-typechain";
+import {
+  makeSmartContractReadCallQueryKey,
+  useSmartContractTransaction,
+} from "@elementfi/react-query-typechain";
+import { queryClient } from "src/elf/queryClient";
+import { useRef } from "react";
+import { ETHERSCAN_TRANSACTION_DOMAIN } from "src/elf-etherscan/domain";
+import { t, jt } from "ttag";
+import ExternalLink from "src/ui/base/ExternalLink/ExternalLink";
+
+export function useChangeDelegationVesting(
+  address: string | null | undefined,
+  signer: Signer | undefined,
+): UseMutationResult<
+  ContractReceipt | undefined,
+  unknown,
+  Parameters<VestingVault["delegate"]>
+> {
+  const toastIdRef = useRef<string>();
+
+  return useSmartContractTransaction(vestingContract, "delegate", signer, {
+    onError: (e) => {
+      toast.error(e.message, {
+        id: toastIdRef.current,
+      });
+    },
+    onTransactionSubmitted: (tx) => {
+      const etherscanLink = (
+        <ExternalLink
+          href={`${ETHERSCAN_TRANSACTION_DOMAIN}/${tx.hash}`}
+          text={t`View on etherscan`}
+          className="text-principalRoyalBlue"
+        />
+      );
+
+      const message = <div>{jt`Changing delegation... ${etherscanLink}`}</div>;
+
+      toastIdRef.current = toast.loading(message);
+    },
+    onTransactionMined: () => {
+      // Invalidate `deposits` so that consumers of `useDelegate` refresh
+      queryClient.invalidateQueries(
+        makeSmartContractReadCallQueryKey(
+          lockingVaultContract.address,
+          "deposits",
+          [address as string],
+        ),
+      );
+
+      toast.success(t`Delegation successfully changed`, {
+        id: toastIdRef.current,
+      });
+    },
+  });
+}
