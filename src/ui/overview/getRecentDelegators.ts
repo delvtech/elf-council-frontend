ea57bf36924af6817ea62d57a07c62d1eecf3bd0
Duke <hnguyen1179@berkeley.edu>
2022-03-31 11:35:25 -0700

    Update block number

--- src/ui/overview/getRecentDelegators.ts
+++ src/ui/overview/getRecentDelegators.ts
@@ -4,6 +4,8 @@ import {
   vestingContract as vestingVault,
 } from "src/elf/contracts";
 
+const STARTING_BLOCK_NUMBER = 14496292;
+
 // Fetches the number of unique delegators from both locking and vesting vaults
 // This function should be used within NextJs getStaticProps with a TTL to cache this result
 export async function getRecentDelegators(): Promise<string[]> {
@@ -12,11 +14,11 @@ export async function getRecentDelegators(): Promise<string[]> {
   const vestingFilter = vestingVault.filters.VoteChange(null, null, null);
 
   const lockingLogs = await provider.getLogs({
-    fromBlock: 0,
+    fromBlock: STARTING_BLOCK_NUMBER,
     ...lockingFilter,
   });
   const vestingLogs = await provider.getLogs({
-    fromBlock: 0,
+    fromBlock: STARTING_BLOCK_NUMBER,
     ...vestingFilter,
   });
 
