f1eb46c4ffc8bccf8e1766aa7be8ad91d3fca5a8
Duke <hnguyen1179@berkeley.edu>
2022-04-04 16:16:45 -0700

    Fix import typo

--- src/ui/base/svg/CoinbaseIcon/CoinbaseIcon.tsx
+++ src/ui/base/svg/CoinbaseIcon/CoinbaseIcon.tsx
@@ -1,6 +1,6 @@
 import { ReactElement } from "react";
 import { SVGComponentProps } from "src/ui/base/svg/types";
-import Svg from "./coinbaseWallet.svg";
+import Svg from "./coinbasewallet.svg";
 
 export default function coinbaseIcon(props: SVGComponentProps): ReactElement {
   return <Svg {...props} />;
