4124418bd56aabf20f68f1d09d295aafe31b9e06
Matthew Brown <mattfdbrown@gmail.com>
2022-04-14 12:20:27 -0500

    subtract foundation and team accounts from circulating vote power calculation (#1059)

--- src/ui/voting/useVotingPowerForProtocol.tsx
+++ src/ui/voting/useVotingPowerForProtocol.tsx
@@ -1,27 +1,41 @@
-import {
-  useSmartContractEvents,
-  useSmartContractReadCall,
-} from "@elementfi/react-query-typechain";
+import { useMemo } from "react";
+
+import { useSmartContractEvents } from "@elementfi/react-query-typechain";
 import { BigNumber, ethers } from "ethers";
 import { formatEther } from "ethers/lib/utils";
-import { useMemo } from "react";
+
 import { addressesJson } from "src/elf-council-addresses";
 import { elementTokenContract, vestingContract } from "src/elf/contracts";
+import { useTokenBalanceOf } from "src/elf/token/useTokenBalanceOf";
+import { useLockingVaultBalance } from "src/ui/voting/useLockingVaultBalance";
 
 const { lockingVault } = addressesJson.addresses;
+
+// the following two addresses have tokens in the locking vault but they aren't used to vote so we
+// need to subtract them from calculation of the circulating voting power for the protocol:
+const foundationAddress = "0xFEaDB1F18386d0225a38E9c4bD1E9Ac52243dE99"; // foundation non voting contract
+const teamAddress = "0xcC46775f1dB1d697c176ed66698BA3C15394C3D4"; // team non voting contract
 
 export function useVotingPowerForProtocol(): number {
   // value in Eth
   const vestingVaultVotingPower = useVestingVaultVotingPower();
 
-  const { data: lockingVaultBalanceBN = BigNumber.from(0) } =
-    useSmartContractReadCall(elementTokenContract, "balanceOf", {
-      callArgs: [lockingVault],
-    });
+  const { data: lockingVaultBalanceBN } = useTokenBalanceOf(
+    elementTokenContract,
+    lockingVault,
+  );
+  const lockingVaultVotingPower = +formatEther(lockingVaultBalanceBN || 0);
 
-  const lockingVaultVotingPower = +formatEther(lockingVaultBalanceBN);
+  // get deposits for foundation and team in the locking vault.  they are delegated to the
+  // 0x0000...0001 address so they can't vote so we need to remove them from the total
+  const foundationBalance = useLockingVaultBalance(foundationAddress);
+  const teamBalance = useLockingVaultBalance(teamAddress);
 
-  const votingPower = +vestingVaultVotingPower + lockingVaultVotingPower;
+  const votingPower =
+    +vestingVaultVotingPower +
+    lockingVaultVotingPower -
+    +foundationBalance -
+    +teamBalance;
 
   return votingPower;
 }
@@ -32,7 +46,7 @@ function useVestingVaultVotingPower(): string {
     "VoteChange",
   );
 
-  return useMemo(() => {
+  const result = useMemo(() => {
     if (!events) {
       return "0";
     }
@@ -64,4 +78,6 @@ function useVestingVaultVotingPower(): string {
     // events are immutable, only update if there are new events.
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [events?.length]);
+
+  return result;
 }
