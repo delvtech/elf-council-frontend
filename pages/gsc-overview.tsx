3acc9bd49e7ad2bb94b72b3406602824cc6c615c
Matthew Brown <mattfdbrown@gmail.com>
2022-04-22 14:48:23 -0500

    updae gsc-overview page

--- pages/gsc-overview.tsx
+++ pages/gsc-overview.tsx
@@ -1,29 +1,15 @@
-import OverviewPage from "src/ui/overview/OverviewPage";
 import React, { ReactElement } from "react";
-import PageView from "src/ui/app/PageView";
-import { ProposalsJson } from "@elementfi/elf-council-proposals";
-import { PROPOSALS_JSON_URL } from "src/elf-council-proposals";
 
-interface HomeProps {
-  proposalsJson: ProposalsJson;
-}
-export default function Home({ proposalsJson }: HomeProps): ReactElement {
+import PageView from "src/ui/app/PageView";
+import GSCOverviewPage from "src/ui/overview/GSCOverviewPage";
+
+interface GSCOverviewProps {}
+export default function GSCOverview(
+  unusedProps: GSCOverviewProps,
+): ReactElement {
   return (
     <PageView childrenContainerClassName="flex justify-center">
-      <OverviewPage proposalsJson={proposalsJson} />
+      <GSCOverviewPage />
     </PageView>
   );
-}
-
-export async function getStaticProps(): Promise<{
-  props: { proposalsJson: ProposalsJson };
-  revalidate: number;
-}> {
-  const res = await fetch(PROPOSALS_JSON_URL);
-  const proposalsJson = await res.json();
-
-  return {
-    props: { proposalsJson },
-    revalidate: 60, // seconds
-  };
 }
